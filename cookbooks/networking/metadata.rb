85afca4c3cad81a6478c4fc6e53352e273f688be
isel <isel_77@hotmail.com>
2011-11-07 22:19:12 -0500

    adding metadata

---
+++ cookbooks/networking/metadata.rb
@@ -0,0 +1,11 @@
+maintainer       "Isel Fernandez"
+maintainer_email "isel_77@hotmail.com"
+license          "our license"
+description      "Manipulates network services"
+long_description ""
+version          "0.0.1"
+
+supports "windows"
+
+recipe "networking::register_host_ip", "Registers a name/ip in the hosts file"
+
