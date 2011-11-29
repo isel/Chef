5c44e8b363a0f067adcc304fb606038c8e23deca
isel <isel_77@hotmail.com>
2011-11-29 15:10:43 -0500

    adding recipe to deploy elastic search

--- cookbooks/deploy/metadata.rb
+++ cookbooks/deploy/metadata.rb
@@ -8,10 +8,21 @@ version          "0.0.1"
 supports "ubuntu"
 
 recipe "deploy::build_scripts", "Deploys the build scripts"
+recipe "deploy::elastic_search", "Deploys ElasticSearch"
 
 attribute "deploy/revision",
   :display_name => "Revision",
   :description => "Revision to install",
+  :required => "required",
+  :recipes => ["deploy::build_scripts"]
+
+attribute "deploy/access_key_id",
+  :display_name => "AWS access key id",
+  :required => "required",
+  :recipes => ["deploy::build_scripts"]
+
+attribute "deploy/secret_access_key",
+  :display_name => "AWS secret access key",
   :required => "required",
   :recipes => ["deploy::build_scripts"]
 
---
+++ generate_json.bat
@@ -0,0 +1,2 @@
+rem %1 is the name of the cookbook for which we want to generate the metadata.json file
+knife cookbook metadata %1 -o c:/projects/github/Chef/cookbooks
