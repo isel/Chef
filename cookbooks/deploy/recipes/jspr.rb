6d643b3ab13687761dcd99831ac409658f3e543f
Isel Fernandez <isel_77@hotmail.com>
2011-12-02 15:31:50 -0500

    fixed code to copy jspr and compass folders

--- cookbooks/deploy/recipes/jspr.rb
+++ cookbooks/deploy/recipes/jspr.rb
@@ -2,8 +2,7 @@ bash 'Deploying websites' do
   code <<-EOF
     rm --recursive --force /var/www/JSPR
     rm --recursive --force /var/www/Compass
-    cp /DeployScripts/JSPR/JSPR/* /var/www/JSPR
-    cp /DeployScripts/JSPR/Compass/* /var/www/Compass
+    cp -r /DeployScripts/JSPR/* /var/www
     ln -s /var/www/JSPR /var/www/Compass/JSPR
   EOF
 end
