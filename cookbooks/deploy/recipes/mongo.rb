d0eca5049b51f86ac278cf66da328b2efa39b69a
Isel Fernandez <isel_77@hotmail.com>
2011-12-02 11:38:22 -0500

    registering mongo as a service

--- cookbooks/deploy/recipes/mongo.rb
+++ cookbooks/deploy/recipes/mongo.rb
@@ -1,6 +1,25 @@
-template "/etc/mongo_test" do
-  source "mongo.erb"
-  variables(
-    :port => node[:deploy][:mongo_port]
-  )
+Chef::Log.info('Registering and/or starting Mongo service')
+
+if !File.exists?('/etc/init.d/mongo')
+  template '/etc/init.d/mongo' do
+    source 'mongo.erb'
+    mode 0755
+    variables(
+      :port => node[:deploy][:mongo_port]
+    )
+  end
+
+  bash 'Registering mongo service' do
+    code <<-EOF
+      update-rc.d mongo defaults
+    EOF
+  end
+else
+  Chef::Log.info('Mongo service is already registered.')
+end
+
+bash 'Starting mongo service' do
+  code <<-EOF
+    service mongo start
+  EOF
 end
