5bdf237cd305bd1424c239cf1dc1716e66990f62
Isel Fernandez <isel_77@hotmail.com>
2011-12-05 15:19:20 -0500

    downloading app server artifacts

--- cookbooks/deploy/recipes/download_artifacts.rb
+++ cookbooks/deploy/recipes/download_artifacts.rb
@@ -1,14 +1,13 @@
-
 template '/RubyScripts/download_artifacts.rb' do
   source 'download_artifacts.erb'
-end
-
-bash "Downloading artifacts" do
   variables(
     :revision => node[:deploy][:revision],
     :access_key_id => node[:deploy][:aws_access_key_id],
     :secret_access_key => node[:deploy][:aws_secret_access_key]
   )
+end
+
+bash "Downloading artifacts" do
   code <<-EOF
     ruby '/RubyScripts/download_artifacts.rb'
   EOF
