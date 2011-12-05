990f2bedd2e81e3395b81f6e8e932c2a5bc22dce
Robert Rehberg <robertrehberg@bellsouth.net>
2011-12-05 10:57:16 -0500

    Protecting the variable assignment of the revision

--- cookbooks/deploy/recipes/jspr.rb
+++ cookbooks/deploy/recipes/jspr.rb
@@ -10,7 +10,8 @@ bash 'Deploying websites' do
 end
 
 versioned_folder = FileList['/DeployScripts/JSPR/JSPR/ver*'][0]
-rev = versioned_folder.split('ver')[1] || '42'
+rev = versioned_folder.split('ver')[1] unless versioned_folder.nil?
+rev ||= '42'
 
 template '/var/www/Compass/settings.js' do
   source 'compass_settings.erb'
