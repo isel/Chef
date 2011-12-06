b768cab05e4a287d0bbeb5717c6b9baeb32e1491
Isel Fernandez <isel_77@hotmail.com>
2011-12-06 11:15:34 -0500

    fixing copying

--- cookbooks/deploy/recipes/foundation_services.rb
+++ cookbooks/deploy/recipes/foundation_services.rb
@@ -10,7 +10,7 @@ end
 #  end
 #end
 
-#FileUtils.cp_r('/DeployScripts/AppServer/Models', '/Websites')
-#FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Gateway.Active/.', '/Websites/ActiveSTS')
-#FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Services/.', '/Websites/Services')
-#FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Services/.', '/Websites/Services.Help')
+FileUtils.cp_r('/DeployScripts/AppServer/Models', '/Websites')
+FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Gateway.Active/.', '/Websites/ActiveSTS')
+FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Services/.', '/Websites/Services')
+FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Services/.', '/Websites/Services.Help')
