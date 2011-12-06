c3829617bd1501c87d14af0aa05a3c5c24e01d6e
Isel Fernandez <isel_77@hotmail.com>
2011-12-06 11:41:30 -0500

    updating  configurations

--- cookbooks/deploy/recipes/foundation_services.rb
+++ cookbooks/deploy/recipes/foundation_services.rb
@@ -4,13 +4,20 @@ FileList['/Websites/**'].each do |f|
   FileUtils.remove_entry(f, true)
 end
 
-#['ActiveSTS', 'Models', 'Services', 'Services.Help'].each do |dir|
-#  directory "/Websites/#{dir}" do
-#    action :create
-#  end
-#end
-
 FileUtils.cp_r('/DeployScripts/AppServer/Models', '/Websites')
 FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Gateway.Active/.', '/Websites/ActiveSTS')
 FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Services/.', '/Websites/Services')
 FileUtils.cp_r('/DeployScripts/AppServer/Websites/UltimateSoftware.Services/.', '/Websites/Services.Help')
+
+ruby_scripts_dir = '/RubyScripts'
+
+template "#{ruby_scripts_dir}/update_configurations.rb" do
+  source 'scripts/update_configurations.erb'
+  #variables(
+  #  :db_port => node[:deploy][:revision]
+  #)
+end
+
+powershell "Updating configurations" do
+  source("ruby #{ruby_scripts_dir}/update_configurations.rb")
+end
