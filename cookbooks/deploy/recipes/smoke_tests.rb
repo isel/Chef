26dc1378d1e172c562b3a294ac9611b4d0dcc28d
Isel Fernandez <isel_77@hotmail.com>
2011-12-09 10:06:15 -0500

    tuning up smoke tests

--- cookbooks/deploy/recipes/smoke_tests.rb
+++ cookbooks/deploy/recipes/smoke_tests.rb
@@ -3,10 +3,12 @@ ruby_scripts_dir = '/RubyScripts'
 template "#{ruby_scripts_dir}/smoke_tests.rb" do
   source 'scripts/smoke_tests.erb'
   variables(
-    :deployment_name => node[:deploy][:deployment_name]
+    :deployment_name => node[:deploy][:deployment_name],
+    :server_type => node[:core][:server_type]
   )
 end
 
 powershell "Running smoke tests" do
   source("rake --rakefile #{ruby_scripts_dir}/smoke_tests.rb")
+  #source("rake --rakefile #{ruby_scripts_dir}/global_smoke_tests.rb")
 end
