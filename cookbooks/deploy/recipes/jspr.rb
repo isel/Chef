1638cd90938565bfb5d2cf5cd46c744340a67ce4
Robert Rehberg <robertrehberg@bellsouth.net>
2011-12-23 11:01:10 -0500

    UP-3235: Fixed the alias

--- cookbooks/deploy/recipes/jspr.rb
+++ cookbooks/deploy/recipes/jspr.rb
@@ -29,37 +29,37 @@ bash 'Set document root' do
   EOF
 end
 
-#bash 'Deploying websites' do
-#  code <<-EOF
-#    rm --recursive --force /var/www/JSPR
-#    rm --recursive --force /var/www/Compass
-#    rm --recursive --force /var/www/Prios
-#    cp -r #{node['deploy_scripts_dir']}/JSPR/* /var/www
-#    ln -s /var/www/JSPR /var/www/Compass/JSPR
-#  EOF
-#end
-#
-#bash 'Deploying prios' do
-#  code <<-EOF
-#    cp -r #{node['deploy_scripts_dir']}/Prios/* /var/www/Prios
-#  EOF
-#  only_if { File.exists?("#{node['deploy_scripts_dir']}/Prios") }
-#end
-#
-#template '/var/www/Compass/settings.js' do
-#  source 'compass_settings.erb'
-#  variables(
-#    :host => node[:deploy][:app_server]
-#  )
-#end
-#
-#template '/var/www/Prios/Prios.plist' do
-#  mode "0644"
-#  source 'prios_plist.erb'
-#end
-#
-#template '/var/www/Prios/index.html' do
-#  mode "0644"
-#  source 'prios_html.erb'
-#end
-#
+bash 'Deploying websites' do
+  code <<-EOF
+    rm --recursive --force /var/www/JSPR
+    rm --recursive --force /var/www/Compass
+    rm --recursive --force /var/www/Prios
+    cp -r #{node['deploy_scripts_dir']}/JSPR/* /var/www
+    ln -s /var/www/JSPR /var/www/Compass/JSPR
+  EOF
+end
+
+bash 'Deploying prios' do
+  code <<-EOF
+    cp -r #{node['deploy_scripts_dir']}/Prios/* /var/www/Prios
+  EOF
+  only_if { File.exists?("#{node['deploy_scripts_dir']}/Prios") }
+end
+
+template '/var/www/Compass/settings.js' do
+  source 'compass_settings.erb'
+  variables(
+    :host => node[:deploy][:app_server]
+  )
+end
+
+template '/var/www/Prios/Prios.plist' do
+  mode "0644"
+  source 'prios_plist.erb'
+end
+
+template '/var/www/Prios/index.html' do
+  mode "0644"
+  source 'prios_html.erb'
+end
+
