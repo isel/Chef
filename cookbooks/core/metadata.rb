3f4f72f79d0b66e0949d742d6e07ead230412e08
Isel Fernandez <isel_77@hotmail.com>
2012-01-06 14:06:46 -0500

    adding recipe to tag the server hostname

--- cookbooks/core/metadata.rb
+++ cookbooks/core/metadata.rb
@@ -8,6 +8,7 @@ version          "0.0.1"
 supports "ubuntu"
 
 recipe "core::install_gems", "Installs ruby gems"
+recipe "core::tag_server_hostname", "Tags the server host name"
 recipe "core::tag_server_type", "Tags the server type"
 
 attribute "core/server_type",
