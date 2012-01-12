4cbd765ecec78de59bc7537cfe069a4ea26b6834
Isel Fernandez <isel_77@hotmail.com>
2012-01-12 15:51:20 -0500

    22: installing elastic search from s3

--- cookbooks/deploy/recipes/elastic_search.rb
+++ cookbooks/deploy/recipes/elastic_search.rb
@@ -2,15 +2,15 @@ ruby_scripts_dir = node['ruby_scripts_dir']
 
 if !File.exists?('/opt/ElasticSearch')
   install_dir = "elasticsearch-#{node[:deploy][:elastic_search_version]}"
-  #bash 'install elastic search prerequisites' do
-  #  code <<-EOF
-  #    apt-get -y install openjdk-6-jre
-  #    apt-get -y install python-dev
-  #    apt-get -y install python-setuptools
-  #    easy_install -U setuptools
-  #    easy_install pymongo
-  #    EOF
-  #end
+  bash 'install elastic search prerequisites' do
+    code <<-EOF
+      apt-get -y install openjdk-6-jre
+      apt-get -y install python-dev
+      apt-get -y install python-setuptools
+      easy_install -U setuptools
+      easy_install pymongo
+      EOF
+  end
 
   template "#{ruby_scripts_dir}/download_elastic_search.rb" do
     source 'scripts/download_elastic_search.erb'
@@ -28,21 +28,21 @@ if !File.exists?('/opt/ElasticSearch')
     EOF
   end
 
-  #bash 'setup Elastic Search as a service' do
-  #  code <<-EOF
-  #    current/bin/service/elasticsearch install
-  #
-  #    echo 'Set up rcelasticsearch as a shortcut to the service wrapper'
-  #    ln -s `readlink -f current/bin/service/elasticsearch` /usr/local/bin/rcelasticsearch
-  #
-  #    echo 'Configuring Cluster'
-  #    echo "cluster.name: UFCluster" >> /opt/ElasticSearch/current/config/elasticsearch.yml
-  #    sed -i "s@set.default.ES_HOME=@#set.default.ES_HOME=@" /opt/ElasticSearch/current/bin/service/elasticsearch.conf
-  #
-  #    echo 'Starting the service'
-  #    service elasticsearch start
-  #  EOF
-  #end
+  bash 'setup Elastic Search as a service' do
+    code <<-EOF
+      current/bin/service/elasticsearch install
+
+      echo 'Set up rcelasticsearch as a shortcut to the service wrapper'
+      ln -s `readlink -f current/bin/service/elasticsearch` /usr/local/bin/rcelasticsearch
+
+      echo 'Configuring Cluster'
+      echo "cluster.name: UFCluster" >> /opt/ElasticSearch/current/config/elasticsearch.yml
+      sed -i "s@set.default.ES_HOME=@#set.default.ES_HOME=@" /opt/ElasticSearch/current/bin/service/elasticsearch.conf
+
+      echo 'Starting the service'
+      service elasticsearch start
+    EOF
+  end
 else
   log 'Elastic Search is already installed.'
 end
