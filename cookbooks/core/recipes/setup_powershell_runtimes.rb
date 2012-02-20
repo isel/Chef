4613901ac02357580f093801fb92d73d8dadd47a
Serguei Kouzmine <Serguei_Kouzmine@ultimatesoftware.com>
2012-02-20 17:02:56 -0500

    56 test appfabric cache corrected the configuration filenames

--- cookbooks/core/recipes/setup_powershell_runtimes.rb
+++ cookbooks/core/recipes/setup_powershell_runtimes.rb
@@ -1,10 +1,18 @@
 ruby_scripts_dir = node['ruby_scripts_dir']
 Dir.mkdir(ruby_scripts_dir) unless File.exist? ruby_scripts_dir
 
-template "/Windows/sysnative/WindowsPowerShell/v1.0/powershell.exe.xml" do
+template "/Windows/sysnative/WindowsPowerShell/v1.0/powershell.exe.config" do
   source 'powershell.erb'
 end
 
-template "/Windows/sysnative/WindowsPowerShell/v1.0/powershell_ise.exe.xml" do
+template "/Windows/system32/WindowsPowerShell/v1.0/powershell.exe.config" do
+  source 'powershell.erb'
+end
+
+template "/Windows/sysnative/WindowsPowerShell/v1.0/powershell_ise.exe.config" do
+  source 'powershell.erb'
+end
+
+template "/Windows/system32/WindowsPowerShell/v1.0/powershell_ise.exe.config" do
   source 'powershell.erb'
 end
