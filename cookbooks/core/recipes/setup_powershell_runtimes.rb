e80ba3baba0010fb00e57358720313bf06865318
Isel Fernandez <isel_77@hotmail.com>
2012-02-20 16:36:05 -0500

    56 - trying to copy xml config files in the 64 bit system directory

--- cookbooks/core/recipes/setup_powershell_runtimes.rb
+++ cookbooks/core/recipes/setup_powershell_runtimes.rb
@@ -1,13 +1,10 @@
 ruby_scripts_dir = node['ruby_scripts_dir']
 Dir.mkdir(ruby_scripts_dir) unless File.exist? ruby_scripts_dir
 
-template "/Windows/System32/WindowsPowerShell/v1.0/powershell.exe.xml" do
+template "/Windows/sysnative/WindowsPowerShell/v1.0/powershell.exe.xml" do
   source 'powershell.erb'
 end
 
-template "/Windows/System32/WindowsPowerShell/v1.0/powershell_ise.exe.xml" do
+template "/Windows/sysnative/WindowsPowerShell/v1.0/powershell_ise.exe.xml" do
   source 'powershell.erb'
 end
-
-`powershell Copy-Item \\Windows\\SysWow64\\WindowsPowerShell\\v1.0\\powershell.exe.xml \\Windows\\System32\\WindowsPowerShell\\v1.0\\`
-`powershell Copy-Item \\Windows\\SysWow64\\WindowsPowerShell\\v1.0\\powershell_ise.exe.xml \\Windows\\System32\\WindowsPowerShell\\v1.0\\`
