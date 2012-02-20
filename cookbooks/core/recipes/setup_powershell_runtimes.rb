28dd070e4574dab738982b9ceb9e0fe44b40f6ff
Isel Fernandez <isel_77@hotmail.com>
2012-02-20 16:28:43 -0500

    56 - trying to copy xml config files in the 64 bit system directory

--- cookbooks/core/recipes/setup_powershell_runtimes.rb
+++ cookbooks/core/recipes/setup_powershell_runtimes.rb
@@ -8,3 +8,6 @@ end
 template "/Windows/System32/WindowsPowerShell/v1.0/powershell_ise.exe.xml" do
   source 'powershell.erb'
 end
+
+`powershell Copy-Item \\Windows\\SysWow64\\WindowsPowerShell\\v1.0\\powershell.exe.xml \\Windows\\System32\\WindowsPowerShell\\v1.0\\`
+`powershell Copy-Item \\Windows\\SysWow64\\WindowsPowerShell\\v1.0\\powershell_ise.exe.xml \\Windows\\System32\\WindowsPowerShell\\v1.0\\`
