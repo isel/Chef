85b3ab5b27ffae3b9a8961eaad0ff8b1a454a945
Isel Fernandez <isel_77@hotmail.com>
2012-02-21 11:16:42 -0500

    56 - deploying app fabric powershell cmdlets

--- cookbooks/core/recipes/setup_powershell_runtimes.rb
+++ cookbooks/core/recipes/setup_powershell_runtimes.rb
@@ -4,7 +4,7 @@ Dir.mkdir(ruby_scripts_dir) unless File.exist? ruby_scripts_dir
 ["#{node['core']['powershell_x32_dir']}", "#{node['core']['powershell_x64_dir']}"].each do |dir|
   ['powershell', 'powershell_ise'].each do |app|
     template "#{dir}/#{app}.exe.config" do
-      source "#{app}.erb"
+      source 'powershell.erb'
     end
   end
 end
