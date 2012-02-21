f11185a3b079bdd18b1005dd3705b5fc60ca3a67
Isel Fernandez <isel_77@hotmail.com>
2012-02-21 11:35:30 -0500

    56 - deploying app fabric powershell cmdlets

--- cookbooks/deploy/attributes/default.rb
+++ cookbooks/deploy/attributes/default.rb
@@ -1,2 +1,5 @@
 default['ruby_scripts_dir'] = '/RubyScripts'
 default['deploy_scripts_dir'] = '/DeployScripts'
+
+default['powershell_x32_dir'] = '/Windows/system32/WindowsPowerShell/v1.0'
+default['powershell_x64_dir'] = '/Windows/sysnative/WindowsPowerShell/v1.0'
