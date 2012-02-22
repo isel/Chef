7b888a04f1f9965d87e7771349de8b8ee7a1266f
Isel Fernandez <isel_77@hotmail.com>
2012-02-22 09:41:15 -0500

    56 - removing elastic search reindex from provision recipe

--- cookbooks/deploy/recipes/provision.rb
+++ cookbooks/deploy/recipes/provision.rb
@@ -14,8 +14,3 @@ end
 powershell "Provisioning data" do
   source("ruby #{ruby_scripts_dir}/provision.rb")
 end
-
-remote_recipe 'Reindexing elastic search' do
-  recipe 'deploy::reindex_elastic_search'
-  recipients_tags ['server:type=db']
-end
