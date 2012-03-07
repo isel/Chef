7fc0c090aedde83ea716aea5c88c9686fa03ce8b
Isel Fernandez <isel_77@hotmail.com>
2012-03-07 11:32:50 -0500

    78 - set fxcop path

--- cookbooks/teamcity/metadata.rb
+++ cookbooks/teamcity/metadata.rb
@@ -5,5 +5,6 @@ description      "Configures TeamCity"
 long_description ""
 version          "0.0.1"
 
+recipe "teamcity::set_fxcop_path", "Sets fxcop path in the build agent properties file"
 recipe "teamcity::set_ncover_path", "Sets ncover path in the build agent properties file"
 
