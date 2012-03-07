441e61b0098fbfb6c18c827fe16ef7ad5b6853e7
Isel Fernandez <isel_77@hotmail.com>
2012-03-07 11:02:06 -0500

    78 - refactoring recipe

--- cookbooks/teamcity/recipes/set_ncover_path.rb
+++ cookbooks/teamcity/recipes/set_ncover_path.rb
@@ -1,6 +1,3 @@
-ruby_scripts_dir = node['ruby_scripts_dir']
-Dir.mkdir(ruby_scripts_dir) unless File.exist? ruby_scripts_dir
-
 powershell "Set ncover path" do
   parameters (
     {
@@ -9,18 +6,5 @@ powershell "Set ncover path" do
         'value' => 'C\:\Program Files\NCover',
     }
   )
-  powershell_script = <<'POWERSHELL_SCRIPT'
-if (Get-Content $env:properties_file| Select-String "$env:key=" -quiet)
-{
-    Write-Output "Replacing $env:key in agent properties file"
-    (Get-Content ($env:properties_file)) | Foreach-Object {$_ -replace "^$env:key=.+$", ("$env:key=" + $env:value)} | Set-Content  ($env:properties_file)
-}
-else
-{
-    Write-Output "Writing $env:key to properties file"
-    Add-Content $env:properties_file "`n"
-    Add-Content $env:properties_file "$env:key=$env:value"
-}
-POWERSHELL_SCRIPT
-  source(powershell_script)
+  source(node['set_value_in_properties_file__powershell_script'])
 end
