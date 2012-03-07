bb5a5d8de37bc4d27fc22a9efc446fad6f94fb61
Isel Fernandez <isel_77@hotmail.com>
2012-03-07 14:48:15 -0500

    78 - fixing paths

--- cookbooks/teamcity/recipes/set_fxcop_path.rb
+++ cookbooks/teamcity/recipes/set_fxcop_path.rb
@@ -3,7 +3,7 @@ powershell "Set fxcop path" do
     {
         'properties_file' => node['properties_file'],
         'key' => 'system.FxCopRoot',
-        'value' => 'C\:\Program Files (x86)\Microsoft Visual Studio 10.0\Team Tools\Static Analysis Tools\FxCop',
+        'value' => 'C\:\Program Files (x86)\\Microsoft Visual Studio 10.0\\Team Tools\\Static Analysis Tools\\FxCop',
     }
   )
   source(node['set_value_in_properties_file_powershell_script'])
