2ac9f5af947b0a9c0b6355f366eaf7179ffe2a4b
Isel Fernandez <isel_77@hotmail.com>
2012-03-07 11:56:37 -0500

    78 - setting gallio and ruby path

--- cookbooks/teamcity/metadata.rb
+++ cookbooks/teamcity/metadata.rb
@@ -6,5 +6,11 @@ long_description ""
 version          "0.0.1"
 
 recipe "teamcity::set_fxcop_path", "Sets fxcop path in the build agent properties file"
+recipe "teamcity::set_gallio_path", "Sets gallio path in the build agent properties file"
 recipe "teamcity::set_ncover_path", "Sets ncover path in the build agent properties file"
+recipe "teamcity::set_ruby_path", "Sets ruby path in the build agent properties file"
 
+attribute "teamcity/gallio_path",
+  :display_name => "gallio path",
+  :required => "required",
+  :recipes => ["teamcity::set_gallio_path"]
---
+++ cookbooks/teamcity/recipes/set_gallio_path.rb
@@ -0,0 +1,10 @@
+powershell "Set gallio path" do
+  parameters (
+    {
+        'properties_file' => node['properties_file'],
+        'key' => 'env.GallioPath',
+        'value' => node[:teamcity][:gallio_path],
+    }
+  )
+  source(node['set_value_in_properties_file_powershell_script'])
+end
