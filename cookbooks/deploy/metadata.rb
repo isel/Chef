f1a3fded58625351f77d9df0eca22a0d0587a45e
Serguei Kouzmine <Serguei_Kouzmine@ultimatesoftware.com>
2012-03-14 14:11:21 -0400

    76 Add messaging server to DIY template - separated launch_mule

--- cookbooks/deploy/metadata.rb
+++ cookbooks/deploy/metadata.rb
@@ -14,6 +14,7 @@ recipe "deploy::appfabric_powershell", "Deploys AppFabric Powershell cmdlets"
 recipe "deploy::appfabric_ensure_is_up", "Ensures AppFabric cache are working"
 recipe "deploy::download_artifacts", "Downloads artifacts"
 recipe "deploy::elastic_search", "Deploys ElasticSearch"
+recipe "deploy::launch_mule.rb", "Launches Mule"
 recipe "deploy::mule", "Deploys Mule ESB"
 recipe "deploy::engine", "Deploys Engine"
 recipe "deploy::foundation_services", "Deploys the foundation rest services"
@@ -151,6 +152,12 @@ attribute "deploy/activemq_version",
   :required => "optional",
   :default => "5.4.3",
   :recipes => ["deploy::activemq"]
+
+attribute "deploy/ulimit_files",
+  :display_name => "setting for log4j",
+  :required => "optional",
+  :default => "8192",
+  :recipes => ["deploy::adjust_ulimit"]
 
 attribute "deploy/revision",
   :display_name => "revision",
--- cookbooks/deploy/recipes/mule.rb
+++ cookbooks/deploy/recipes/mule.rb
@@ -14,7 +14,7 @@ bash 'install maven' do
     mvn  --version
     sudo apt-get -y install tomcat6
   EOF
-done
+end
 # openjdk
 
 # need to do it once
@@ -31,18 +31,18 @@ end
 # TODO -  detect version file
 # NOTE: mule is 180MB
 if !File.exists?('/opt/mule/bin')
-bash 'install mule' do
-   code <<-EOF
-   mkdir ~/Installs
-   cd ~/Installs
-   # initial version - upload 200 MB from the web
-   wget http://s3.amazonaws.com/MuleEE/mule-ee-distribution-standalone-mmc-#{version}.tar.gz
-   tar xf mule-ee-distribution-standalone-mmc-#{version}.tar.gz
-   mkdir /opt/mule
-   pushd /opt/mule
-   cp -r ~/Installs/mule-enterprise-standalone-#{version}/* .
-   chmod -R 777 .
-   EOF
+  bash 'install mule' do
+    code <<-EOF
+    mkdir ~/Installs
+    cd ~/Installs
+    # initial version - upload 200 MB from the web
+    wget http://s3.amazonaws.com/MuleEE/mule-ee-distribution-standalone-mmc-#{version}.tar.gz
+    tar xf mule-ee-distribution-standalone-mmc-#{version}.tar.gz
+    mkdir /opt/mule
+    pushd /opt/mule
+    cp -r ~/Installs/mule-enterprise-standalone-#{version}/* .
+    chmod -R 777 .
+  EOF
 end
 else
   log 'Mule already installed.'
@@ -64,27 +64,4 @@ EOF
 end
 else
   log 'maven repositories already populated.'
-end
-
-# the script may fail with
-# jar hell error with log4j
-# http://stackoverflow.com/questions/1359708/problem-using-log4j-with-axis2
-#
-#
-#  /opt/mule/lib/boot/log4j-1.2.14.jar
-#  /opt/mule/apps/mmc/webapps/mmc/WEB-INF/lib/slf4j-log4j12-1.5.6.jar
-#  /opt/mule/apps/mmc/webapps/mmc/WEB-INF/lib/log4j-1.2.13.jar
-#  /opt/ElasticSearch/elasticsearch-0.17.6/lib/log4j-1.2.16.jar
-#
-# and other errors
-
-bash 'launch mule' do
-      code <<-EOF
-      cd /opt/mule/bin
-      . /etc/bash.bashrc
-      if [ -x mule ] ; then
-        ./mule
-      fi
-      wget -O /dev/null http://node['ipaddress']:8585/mmc
-  EOF
 end
