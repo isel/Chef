28a0e5d3aa4cada5bb3a9fce2f03f2322e069dba
Serguei Kouzmine <Serguei_Kouzmine@ultimatesoftware.com>
2012-03-14 14:50:33 -0400

    76 Add messaging server to DIY template - activemq

--- cookbooks/deploy/recipes/activemq.rb
+++ cookbooks/deploy/recipes/activemq.rb
@@ -7,18 +7,22 @@ bash 'download ActiveMQ' do
   code <<-EOF
   mkdir ~/Installs
   cd ~/Installs
-  wget http://apache.mirrors.redwire.net/activemq/apache-activemq/#{version}/apache-activemq-#{version}-bin.tar.gz
+  wget --quiet http://apache.mirrors.redwire.net/activemq/apache-activemq/#{version}/apache-activemq-#{version}-bin.tar.gz
   tar xf apache-activemq-#{version}-bin.tar.gz
   mkdir /opt/activemq
   pushd /opt/activemq
   cp -r ~/Installs/apache-activemq-#{version}/* .
   chmod -R 777 .
+  if [ ! -f /opt/activemq/bin/activemq ]
+  then
+    exit 1
+  fi
 EOF
 end
 bash 'launch ActiveMQ' do
   code <<-EOF
   pushd /opt/activemq/bin
-  nohup ./activemq start > /var/log/smlog 2>&1 &
+  /usr/bin/nohup ./activemq start > /var/log/smlog 2>&1 &
   netstat -an | grep :61616
 EOF
 end
