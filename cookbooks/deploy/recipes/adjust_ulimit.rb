58353b339ae01542a8e6f7d3547a14f5b0df9e22
Serguei Kouzmine <Serguei_Kouzmine@ultimatesoftware.com>
2012-03-14 17:51:28 -0400

    76 Add messaging server to DIY template - fixing generated bash scripts

--- cookbooks/deploy/recipes/adjust_ulimit.rb
+++ cookbooks/deploy/recipes/adjust_ulimit.rb
@@ -12,7 +12,7 @@ ULIMIT_FILES=`expr 0  + $ULIMIT_FILES`
 if [ $ULIMIT_FILES -lt #{ulimit_files} ] ; then
   cp /etc/security/limits.conf /tmp/security_limits_conf.$$
   ADD_SETTING="nofile #{ulimit_files}"
-  sed  -i -e 's/\(# End of file\)/nofile #{ulimit_files}\\n\\1/' /tmp/security_limits_conf.$$
+  sed  -i -e 's/\\(# End of file\\)/nofile #{ulimit_files}\\n\\1/' /tmp/security_limits_conf.$$
   cp /tmp/security_limits_conf.$$ /etc/security/limits.conf
   # TODO - what service to restart to make changes take effect w/o reboot / logoff .
   # maybe nothing - the limits.conf is used to persist across users.
