115975233be010a0fa63c65a06c504c716e1ccdc
Isel Fernandez <isel_77@hotmail.com>
2012-03-15 16:49:07 -0400

    88 - pulling artifacts from specified folder in s3

--- cookbooks/deploy/recipes/download_binaries.rb
+++ cookbooks/deploy/recipes/download_binaries.rb
@@ -1,25 +1,25 @@
 ruby_scripts_dir = node['ruby_scripts_dir']
 Dir.mkdir(ruby_scripts_dir) unless File.exist? ruby_scripts_dir
 
-template "#{ruby_scripts_dir}/download_artifacts.rb" do
+template "#{ruby_scripts_dir}/download_binaries.rb" do
   source 'scripts/download_artifacts.erb'
   variables(
     :aws_access_key_id => node[:deploy][:aws_access_key_id],
     :aws_secret_access_key => node[:deploy][:aws_secret_access_key],
-    :artifacts => node[:deploy][:artifacts],
-    :revision => node[:deploy][:revision],
-    :s3_folder => ''
+    :artifacts => node[:deploy][:binaries_artifacts],
+    :revision => node[:deploy][:binaries_revision],
+    :s3_folder => 'Binaries'
   )
 end
 
 if node[:platform] == "ubuntu"
   bash 'Downloading artifacts' do
     code <<-EOF
-      ruby #{ruby_scripts_dir}/download_artifacts.rb
+      ruby #{ruby_scripts_dir}/download_binaries.rb
     EOF
   end
 else
   powershell "Downloading artifacts" do
-    source("ruby #{ruby_scripts_dir}/download_artifacts.rb")
+    source("ruby #{ruby_scripts_dir}/download_binaries.rb")
   end
 end
