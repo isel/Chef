e5bd99801f33ba33b6c793f913a9d7b459d593b5
Serguei Kouzmine <Serguei_Kouzmine@ultimatesoftware.com>
2012-03-20 14:58:19 -0400

    59 sarmus logging hurts performance tests

--- cookbooks/deploy/recipes/sarmus_restart.rb
+++ cookbooks/deploy/recipes/sarmus_restart.rb
@@ -1,8 +1,8 @@
-@sarmus_loglevel = node[:deploy][:sarmus_loglevel]
-script "Restart sarmus to #{@sarmus_loglevel}" do
+sarmus_loglevel = node[:deploy][:sarmus_loglevel]
+script "Restart sarmus to #{sarmus_loglevel}" do
   interpreter "bash"
   code <<-EOF
-    echo "Restart sarmus to #{@sarmus_loglevel}"
+    echo "Restart sarmus to #{sarmus_loglevel}"
     SERVICE_STATUS=`service sarmus_service status`
     expr "$SERVICE_STATUS" : '.*running.*' > /dev/null
     STATUS=$?
@@ -12,7 +12,7 @@ script "Restart sarmus to #{@sarmus_loglevel}" do
       echo 'sarmus service was not operational'
     fi
       echo 'updating sarmus_service file'
-      sed -e 's/\\(SARMUS_LOGLEVEL\\)=\\([0-9]\\)/\\1=#{@sarmus_loglevel}/' /etc/init.d/sarmus_service  > /tmp/sarmus_service.tmp.$$
+      sed -e 's/\\(SARMUS_LOGLEVEL\\)=\\([0-9]\\)/\\1=#{sarmus_loglevel}/' /etc/init.d/sarmus_service  > /tmp/sarmus_service.tmp.$$
       cp /tmp/sarmus_service.tmp.$$ /etc/init.d/sarmus_service
       service sarmus_service start
    EOF
