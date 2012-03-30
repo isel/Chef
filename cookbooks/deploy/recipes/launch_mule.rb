4534100af85687a3c0e93fcc7ba81749b1ee6f07
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-03-30 10:31:50 -0400

    76 - Add messaging server to DIY template - defensive bash script

--- cookbooks/deploy/recipes/launch_mule.rb
+++ cookbooks/deploy/recipes/launch_mule.rb
@@ -14,8 +14,21 @@ bash 'launch mule' do
       export MAVEN_HOME=/usr/share/maven2
       export MAVEN_OPTS='-Xmx512m -XX:MaxPermSize=256m'
       export PATH=$PATH:$MULE_HOME/bin:$JAVA_HOME/bin
-      ulimit -n #{ulimit_files}
+      FILE_LIMIT=#{ulimit_files}
+      if [ ! -z $FILE_LIMIT ];      then
+      FILE_LIMIT=`expr $FILE_LIMIT : '^\\([0-9][0-9]*\\)$'`
+      echo "FILE_LIMIT='$FILE_LIMIT'"
+      fi
+      if [ ! -z $FILE_LIMIT ]
+      then
+        echo 'bumping open file handle limit to $FILE_LIMIT'
+        ulimit -n $FILE_LIMIT
+      else
+        echo 'no meaningful FILE_LIMIT provided'
+      fi
+      ulimit -a
       if [ -x mule ] ; then
+        echo 'starting the mule'
         /usr/bin/nohup ./mule start
       fi
   EOF
