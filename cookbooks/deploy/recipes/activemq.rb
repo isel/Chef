22f4a11261911fdc413895cb97e2beeefb710468
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-04-11 16:44:05 -0400

    94 - internal repository for external vendors  - updated from spec test

--- cookbooks/deploy/recipes/activemq.rb
+++ cookbooks/deploy/recipes/activemq.rb
@@ -1,13 +1,17 @@
+# TODO - skip action when directory already exists.
+
 ruby_scripts_dir = node['ruby_scripts_dir']
 version  = node[:deploy][:activemq_version]
+product = 'activemq'
 template "#{ruby_scripts_dir}/download_vendor_drop.rb" do
   source 'scripts/download_vendor_drop.erb'
   variables(
     :aws_access_key_id => node[:deploy][:aws_access_key_id],
     :aws_secret_access_key => node[:deploy][:aws_secret_access_key],
-    :product => 'activemq',
+    :product => product,
     :version => version,
     :filelist => 'activemq',
+    :deploy_folder => '/opt',
     :no_explode => '0'
   )
 end
@@ -21,7 +25,9 @@ end
 bash 'Setting directory links' do
   code <<-EOF
   pushd /opt
-  ln -s apache-activemq-#{version} activemq
+  if [ -d  "apache-activemq-#{version} ] ; then
+    ln -s apache-activemq-#{version} #{product}
+  fi
   pushd activemq
   chmod -R 777 .
   if [ ! -f /opt/activemq/bin/activemq ] ; then
