f2b764a1a240729ba4229114795feabd340084d5
Robert Rehberg <robertrehberg@bellsouth.net>
2012-06-09 15:37:08 -0400

    Removing the passenger code as it's been temporarily extracted to a rightscript.  Fixed the passenger apache setup so it now corresponds to the passenger install path.

--- cookbooks/infrastructure/recipes/api.rb
+++ cookbooks/infrastructure/recipes/api.rb
@@ -1,19 +1,3 @@
-bash 'Installing Passenger prereqs' do
-  code <<-EOF
-    gem install passenger -v 3.0.12 --no-rdoc --no-ri
-
-    apt-get install -y libcurl4-openssl-dev
-    apt-get install -y apache2-prefork-dev
-    apt-get install -y libapr1-dev
-    apt-get install -y libaprutil1-dev
-
-    sync
-
-    # this might need the ruby path, see http://weblog.bitlasoft.com/2010/05/installing-passenger-passenger-install-apache2-module-command-not-found/
-    passenger-install-apache2-module --auto
-  EOF
-end
-
 bash 'Set document root and configure Passenger' do
   code <<-EOF
     if grep -q DocumentRoot /etc/apache2/apache2.conf; then
@@ -21,10 +5,10 @@ bash 'Set document root and configure Passenger' do
         exit 0
     fi
 
-    echo LoadModule passenger_module /opt/rvm/gems/ruby-1.9.2-head/gems/passenger-3.0.12/ext/apache2/mod_passenger.so >> /etc/apache2/apache2.conf
-    echo PassengerRoot /opt/rvm/gems/ruby-1.9.2-head/gems/passenger-3.0.12 >> /etc/apache2/apache2.conf
+    echo LoadModule passenger_module /opt/passenger-3.0.12/ext/apache2/mod_passenger.so >> /etc/apache2/apache2.conf
+    echo PassengerRoot /opt/passenger-3.0.12 >> /etc/apache2/apache2.conf
     echo PassengerRuby /opt/rvm/wrappers/ruby-1.9.2-head/ruby >> /etc/apache2/apache2.conf
-    echo PassengerDefaultUser www-data >> /etc/apache2/apache2.conf
+    echo PassengerDefaultUser root >> /etc/apache2/apache2.conf
 
     echo DocumentRoot "/var/www/api/public" >> /etc/apache2/apache2.conf
   EOF
@@ -40,13 +24,14 @@ bash 'Setting up website' do
     cp -r #{node['infrastructure_directory']}/InfrastructureServices/* /var/www/api
 
     echo "" >> /var/www/api/log/production.log
+    echo "" >> /var/www/api/log/rest.log
     chmod --recursive 0666 /var/www/api/log
     chown --recursive www-data:www-data /var/www/api/log
+
 
     cd /var/www/api
     bundle install
 
-    echo restarting apache
     service apache2 restart
   EOF
 end
