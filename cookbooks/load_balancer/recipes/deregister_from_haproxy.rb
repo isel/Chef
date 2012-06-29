87a106283d34aec4498c9e1153472d3881bb99c9
Isel Fernandez <isel_77@hotmail.com>
2012-06-29 14:38:08 -0400

    adding recipe to deregister_from_haproxy

--- cookbooks/load_balancer/recipes/deregister_from_haproxy.rb
+++ cookbooks/load_balancer/recipes/deregister_from_haproxy.rb
@@ -1,8 +1,8 @@
-bash 'Removing instance from haproxy configuration' do
-  code <<-EOF
-    name='LB - disconnect instance from haproxy'
-    parameters="INSTANCE_BACKEND_NAME=text:#{node[:load_balancer][:backend_name]}"
-    tags="lb:prefix=#{node[:load_balancer][:prefix]}"
-    rs_run_right_script --name "#{name}" --parameter "#{parameters}" --recipient_tags "#{tags}"
-  EOF
-end
+#bash 'Removing instance from haproxy configuration' do
+#  code <<-EOF
+name='LB - disconnect instance from haproxy'
+parameters="INSTANCE_BACKEND_NAME=text:#{node[:load_balancer][:backend_name]}"
+tags="lb:prefix=#{node[:load_balancer][:prefix]}"
+rs_run_right_script --name "#{name}" --parameter "#{parameters}" --recipient_tags "#{tags}"
+  #EOF
+#end
