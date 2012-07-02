75ca8c0ea909c6e36880c52a91bcd9703cb57887
Isel Fernandez <isel_77@hotmail.com>
2012-07-02 15:53:06 -0400

    163 - using prefix and forwarding ports to disconnect instances from load balancer

--- cookbooks/load_balancer/recipes/disconnect_instance_from_haproxy.rb
+++ cookbooks/load_balancer/recipes/disconnect_instance_from_haproxy.rb
@@ -1,8 +1,13 @@
-bash 'Removing instance from haproxy configuration' do
-  code <<-EOF
-    script="/opt/rightscale/lb/bin/haproxy_config_server.rb"
-    args="-a del -w -s #{node[:load_balancer][:instance_backend_name]} -l www"
+node[:load_balancer][:forwarding_ports].reject{|port| port == '443'}.each do |port|
+  listener_name = "#{node[:load_balancer][:prefix]}#{port}"
+  bash 'Removing instance from haproxy configuration' do
+    code <<-EOF
+      echo disconnecting instance: #{node[:load_balancer][:instance_backend_name]}, with listener: #{listener_name} from haproxy
 
-    /opt/rightscale/sandbox/bin/ruby $script $args
-  EOF
+      #script="/opt/rightscale/lb/bin/haproxy_config_server.rb"
+      #args="-a del -w -s #{node[:load_balancer][:instance_backend_name]} -l #{listener_name}"
+
+      #/opt/rightscale/sandbox/bin/ruby $script $args
+    EOF
+  end
 end
