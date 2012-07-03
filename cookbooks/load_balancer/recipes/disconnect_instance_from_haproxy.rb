5c3dce41de1f24813405fe78e692f078d74aa02b
Isel Fernandez <isel_77@hotmail.com>
2012-07-03 10:38:31 -0400

    162 - done with disconnect_instance_from_haproxy

--- cookbooks/load_balancer/recipes/disconnect_instance_from_haproxy.rb
+++ cookbooks/load_balancer/recipes/disconnect_instance_from_haproxy.rb
@@ -5,10 +5,10 @@ forwarding_ports.each do |port|
     code <<-EOF
       echo disconnecting instance: #{node[:load_balancer][:instance_backend_name]}, with listener: #{listener_name} from haproxy
 
-      #script="/opt/rightscale/lb/bin/haproxy_config_server.rb"
-      #args="-a del -w -s #{node[:load_balancer][:instance_backend_name]} -l #{listener_name}"
+      script="/opt/rightscale/lb/bin/haproxy_config_server.rb"
+      args="-a del -w -s #{node[:load_balancer][:instance_backend_name]} -l #{listener_name}"
 
-      #/opt/rightscale/sandbox/bin/ruby $script $args
+      /opt/rightscale/sandbox/bin/ruby $script $args
     EOF
   end
 end
