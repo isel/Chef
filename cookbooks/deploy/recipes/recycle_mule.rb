1a34cdaa14339ae7f8d65719c89a7c2d899cfccd
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-07-12 15:29:28 -0400

    Mule 3.3.0 ActiveMQ 5.6 -  fully recycle mule and scorch orphaned processes

--- cookbooks/deploy/recipes/recycle_mule.rb
+++ cookbooks/deploy/recipes/recycle_mule.rb
@@ -1,4 +1,6 @@
 # Stop and remove Mule_ESB
+require 'fileutils'
+require 'yaml'
 
 version = node[:deploy][:mule_version]
 complete_removal = 1
@@ -10,9 +12,19 @@ bash 'Stop mule service' do
     MULE_LAUNCHER=`ps ax |  grep /mule/lib/boot/exec`
     if [ ! -z "$MULE_LAUNCHER" ] ; then
       echo 'Stopping the mule'
+      if [ -d "/opt/mule/bin" ] ; then
       pushd /opt/mule/bin
       ./mule stop
       popd
+      else
+       echo "detected orphaned mule process running but without mule directory"
+        SERVICE_PROCESS=`ps ax | grep mule | grep -v grep | awk '{print $1}'|tail -1`
+        while [ ! -z $SERVICE_PROCESS ] ; do
+        echo "Killing the service process $SERVICE_PROCESS"
+        kill -KILL  $SERVICE_PROCESS
+        sleep 1
+        SERVICE_PROCESS=`ps ax | grep mule | grep -v grep | awk '{print $1}'|tail -1`
+        done
     else
       echo 'Mule service is not running'
     fi
@@ -26,7 +38,7 @@ if File.exists?('/opt/mule')
     set +e
     COMPLETE_REMOVAL="#{complete_removal}"
     if  [ "$COMPLETE_REMOVAL" == "1" ] ; then
-      rm $HOME/Installs/mule-enterprise-standalone-#{version}
+      rm /opt/mule-enterprise-standalone-#{version}
     fi
     rm -rf /opt/mule
     EOF
