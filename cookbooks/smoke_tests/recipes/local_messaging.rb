322ea0fa7dc1ae62c190f14661404768ef73eb4d
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-07-18 16:28:46 -0400

    Mule 3.3.0 ActiveMQ 5.6.0 finalize the move to standard smoke tests bootstrapping  from CI area - removed cookbook scoped alternative

--- cookbooks/smoke_tests/recipes/local_messaging.rb
+++ cookbooks/smoke_tests/recipes/local_messaging.rb
@@ -29,31 +29,3 @@ bash 'Running local smoke tests' do
     rake --rakefile #{ruby_scripts_dir}/local_messaging.rb
   EOF
 end
-
-
-template "#{ruby_scripts_dir}/validate_configuration_tokens.rb" do
-   source 'scripts/validate_configuration_tokens.erb'
-   variables(
-
-     :db_server => node[:deploy][:db_server],
-     :db_port => node[:deploy][:db_port],
-     :appserver => node[:deploy][:app_server],
-     :app_server => node[:deploy][:app_server],
-     :search_port => node[:deploy][:elastic_search_port],
-     :search_server => node[:deploy][:search_server],
-     :messaging_server_port => node[:deploy][:messaging_server_port],
-     :engine_server => node[:deploy][:engine_server],
-     :cache_server => node[:deploy][:cache_server],
-     :messaging_server => node[:deploy][:messaging_server],
-     :engine_port => node[:deploy][:engine_port],
-     :web_server => node[:deploy][:web_server]
-     # no trailing comma
-
-   )
- end
-
- bash 'Validating tokens in Mule configuration' do
-    code <<-EOF
-     rake --rakefile #{ruby_scripts_dir}/validate_configuration_tokens.rb
-   EOF
- end
