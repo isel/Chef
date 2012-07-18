65605781fd1b214491a495870cf45fb7f2e4719c
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-07-18 16:10:46 -0400

    Mule 3.3.0 ActiveMQ 5.6.0 fix the RS inouts to erb template macros to environment variable conversion error

--- cookbooks/smoke_tests/recipes/local_messaging.rb
+++ cookbooks/smoke_tests/recipes/local_messaging.rb
@@ -5,7 +5,22 @@ template "#{ruby_scripts_dir}/local_messaging.rb" do
   variables(
     :mule_port => node[:deploy][:mule_port],
     :activemq_port => node[:deploy][:activemq_port],
-    :server_type => node[:core][:server_type]
+    :server_type => node[:core][:server_type] ,
+
+    :db_server => node[:deploy][:db_server],
+    :db_port => node[:deploy][:db_port],
+    :appserver => node[:deploy][:app_server],
+    :app_server => node[:deploy][:app_server],
+    :search_port => node[:deploy][:elastic_search_port],
+    :search_server => node[:deploy][:search_server],
+    :messaging_server_port => node[:deploy][:messaging_server_port],
+    :engine_server => node[:deploy][:engine_server],
+    :cache_server => node[:deploy][:cache_server],
+    :messaging_server => node[:deploy][:messaging_server],
+    :engine_port => node[:deploy][:engine_port],
+    :web_server => node[:deploy][:web_server]
+    # no trailing comma
+
   )
 end
 
