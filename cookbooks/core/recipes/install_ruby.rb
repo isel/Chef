a2f3c32940c7d4d54cbbadde6fca560a48e636aa
Isel Fernandez <isel_77@hotmail.com>
2012-07-31 10:35:58 -0400

    168 - installing ruby from source

--- cookbooks/core/recipes/install_ruby.rb
+++ cookbooks/core/recipes/install_ruby.rb
@@ -7,13 +7,13 @@ cookbook_file "#{ruby_scripts_dir}/download_vendor_drop.erb" do
   cookbook "deploy"
 end
 
-template "#{ruby_scripts_dir}/download_ruby.rb" do
-  source "#{ruby_scripts_dir}/download_vendor_drop.erb"
-  variables(
-    :aws_access_key_id => node[:deploy][:aws_access_key_id],
-    :aws_secret_access_key => node[:deploy][:aws_secret_access_key],
-    :product => 'ruby',
-    :version => '1.9.2-p320',
-    :filelist => 'ruby'
-  )
-end
+#template "#{ruby_scripts_dir}/download_ruby.rb" do
+#  source "#{ruby_scripts_dir}/download_vendor_drop.erb"
+#  variables(
+#    :aws_access_key_id => node[:deploy][:aws_access_key_id],
+#    :aws_secret_access_key => node[:deploy][:aws_secret_access_key],
+#    :product => 'ruby',
+#    :version => '1.9.2-p320',
+#    :filelist => 'ruby'
+#  )
+#end
