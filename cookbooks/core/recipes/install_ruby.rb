4df5e6b3f7f55feb19f54370d677cc8b20c968a0
Isel Fernandez <isel_77@hotmail.com>
2012-07-31 13:58:09 -0400

    168 - downloading ruby

--- cookbooks/core/recipes/install_ruby.rb
+++ cookbooks/core/recipes/install_ruby.rb
@@ -8,7 +8,8 @@ template "#{node['ruby_scripts_dir']}/download_ruby.rb" do
     :aws_secret_access_key => node[:core][:aws_secret_access_key],
     :product => 'ruby',
     :version => '1.9.2-p320',
-    :filelist => 'ruby'
+    :filelist => 'ruby',
+    :deploy_folder => '~/src'
   )
 end
 
@@ -24,3 +25,39 @@ bash 'Download ruby' do
     /opt/rightscale/sandbox/bin/ruby -rubygems #{node['ruby_scripts_dir']}/download_ruby.rb
   EOF
 end
+
+#bash 'Install ruby from source' do
+#  code <<-EOF
+#    apt-get -y install \
+#               build-essential \
+#               libreadline-dev \
+#               libssl-dev \
+#               libyaml-dev \
+#               libffi-dev \
+#               libncurses-dev \
+#               libdb-dev \
+#               libgdbm-dev \
+#               tk-dev
+#
+#    mkdir -p ~/src/
+#    tar xvf ruby-1.9.2-p320.tar.bz2 -C ~/src/
+#    mkdir -p ~/src/ruby-1.9.2-p320-build
+#
+#    cd ~/src/ruby-1.9.2-p320-build
+#
+#    ../ruby-1.9.2-p320/configure \
+#            --enable-shared \
+#            --prefix=/opt/ruby/1.9.2-p320 2>&1 \
+#            | tee log-1-configure.txt
+#
+#    make all 2>&1 | tee log-2-build.txt
+#    make test 2>&1 | tee log-3-test.txt
+#    make install 2>&1 | tee log-4-install.txt
+#
+#    cd /opt/ruby
+#    rm -f active && ln -sf 1.9.2-p320 active
+#
+#    export PATH="/opt/ruby/active/bin:$PATH"
+#    export MANPATH="/opt/ruby/active/share/man"
+#  EOF
+#end
