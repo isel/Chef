bdc1be379d395702c44bdc7dfce21c5516624a93
Isel Fernandez <isel_77@hotmail.com>
2012-07-31 14:22:43 -0400

    168 - installing ruby

--- cookbooks/core/recipes/install_ruby.rb
+++ cookbooks/core/recipes/install_ruby.rb
@@ -1,5 +1,7 @@
 include_recipe 'core::download_vendor_artifacts_prereqs'
 
+ruby_version = '1.9.2-p320'
+
 template "#{node['ruby_scripts_dir']}/download_ruby.rb" do
   local true
   source "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
@@ -7,7 +9,7 @@ template "#{node['ruby_scripts_dir']}/download_ruby.rb" do
     :aws_access_key_id => node[:core][:aws_access_key_id],
     :aws_secret_access_key => node[:core][:aws_secret_access_key],
     :product => 'ruby',
-    :version => '1.9.2-p320',
+    :version => ruby_version,
     :filelist => 'ruby',
     :deploy_folder => '/root/src'
   )
@@ -26,38 +28,34 @@ bash 'Download ruby' do
   EOF
 end
 
-#bash 'Install ruby from source' do
-#  code <<-EOF
-#    apt-get -y install \
-#               build-essential \
-#               libreadline-dev \
-#               libssl-dev \
-#               libyaml-dev \
-#               libffi-dev \
-#               libncurses-dev \
-#               libdb-dev \
-#               libgdbm-dev \
-#               tk-dev
-#
-#    mkdir -p ~/src/
-#    tar xvf ruby-1.9.2-p320.tar.bz2 -C ~/src/
-#    mkdir -p ~/src/ruby-1.9.2-p320-build
-#
-#    cd ~/src/ruby-1.9.2-p320-build
-#
-#    ../ruby-1.9.2-p320/configure \
-#            --enable-shared \
-#            --prefix=/opt/ruby/1.9.2-p320 2>&1 \
-#            | tee log-1-configure.txt
-#
-#    make all 2>&1 | tee log-2-build.txt
-#    make test 2>&1 | tee log-3-test.txt
-#    make install 2>&1 | tee log-4-install.txt
-#
-#    cd /opt/ruby
-#    rm -f active && ln -sf 1.9.2-p320 active
-#
-#    export PATH="/opt/ruby/active/bin:$PATH"
-#    export MANPATH="/opt/ruby/active/share/man"
-#  EOF
-#end
+bash 'Install ruby from source' do
+  code <<-EOF
+    apt-get -y install \
+               build-essential \
+               libreadline-dev \
+               libssl-dev \
+               libyaml-dev \
+               libffi-dev \
+               libncurses-dev \
+               libdb-dev \
+               libgdbm-dev \
+               tk-dev
+
+    cd ~/src/ruby
+
+    ../ruby/configure \
+            --enable-shared \
+            --prefix=/opt/ruby/#{ruby_version} 2>&1 \
+            | tee log-1-configure.txt
+
+    make all 2>&1 | tee log-2-build.txt
+    make test 2>&1 | tee log-3-test.txt
+    make install 2>&1 | tee log-4-install.txt
+
+    cd /opt/ruby
+    rm -f active && ln -sf #{ruby_version} active
+
+    export PATH="/opt/ruby/active/bin:$PATH"
+    export MANPATH="/opt/ruby/active/share/man"
+  EOF
+end
