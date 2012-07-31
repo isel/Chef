4724d90c59272c3a6cccbaa85ee810e52a5d4f8e
Isel Fernandez <isel_77@hotmail.com>
2012-07-31 12:16:24 -0400

    168 - adding prereqs for downloading artifacts

--- cookbooks/core/recipes/install_ruby.rb
+++ cookbooks/core/recipes/install_ruby.rb
@@ -1,9 +1,7 @@
 include_recipe 'core::download_vendor_artifacts_prereqs'
 
-ruby_scripts_dir = node['ruby_scripts_dir']
-
-template "#{ruby_scripts_dir}/download_ruby.rb" do
-  source "#{ruby_scripts_dir}/download_vendor_artifacts.erb"
+template "#{node['ruby_scripts_dir']}/download_ruby.rb" do
+  path "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
   variables(
     :aws_access_key_id => node[:core][:aws_access_key_id],
     :aws_secret_access_key => node[:core][:aws_secret_access_key],
@@ -16,6 +14,6 @@ end
 bash 'Download ruby' do
   code <<-EOF
     gem install fog -v 1.1.1 --no-rdoc --no-ri
-    /opt/rightscale/sandbox/bin/ruby -rubygems #{ruby_scripts_dir}/download_ruby.rb
+    /opt/rightscale/sandbox/bin/ruby -rubygems #{node['ruby_scripts_dir']}/download_ruby.rb
   EOF
 end
