ccdbf2a643e2d8b02c1b94b361dea90aa77787dc
Isel Fernandez <isel_77@hotmail.com>
2012-07-31 11:32:09 -0400

    168 - installing ruby from source

--- cookbooks/core/recipes/install_ruby.rb
+++ cookbooks/core/recipes/install_ruby.rb
@@ -2,10 +2,8 @@ ruby_scripts_dir = node['ruby_scripts_dir']
 Dir.mkdir(ruby_scripts_dir) unless File.exist? ruby_scripts_dir
 
 cookbook_file "#{ruby_scripts_dir}/download_vendor_drop.erb" do
-  source "scripts/download_vendor_drop.erb" # this is the value that would be inferred from the path parameter
+  source "download_vendor_drop.erb" # this is the value that would be inferred from the path parameter
   mode "0644"
-  cookbook "deploy"
-  path "templates"
 end
 
 #template "#{ruby_scripts_dir}/download_ruby.rb" do
