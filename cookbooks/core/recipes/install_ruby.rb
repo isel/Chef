cadeb9a5aeed591b69b0e38d728a759099b5e122
Isel Fernandez <isel_77@hotmail.com>
2012-07-31 10:23:08 -0400

    168 - installing ruby from source

--- cookbooks/core/recipes/install_ruby.rb
+++ cookbooks/core/recipes/install_ruby.rb
@@ -2,7 +2,7 @@ ruby_scripts_dir = node['ruby_scripts_dir']
 Dir.mkdir(ruby_scripts_dir) unless File.exist? ruby_scripts_dir
 
 template "#{ruby_scripts_dir}/download_ruby.rb" do
-  source '../../deploy/templates/scripts/download_vendor_drop.erb'
+  source 'deploy/templates/scripts/download_vendor_drop.erb'
   variables(
     :aws_access_key_id => node[:deploy][:aws_access_key_id],
     :aws_secret_access_key => node[:deploy][:aws_secret_access_key],
