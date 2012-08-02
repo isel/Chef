487762e11396bf9beffcfd8ac50f995f9fad9dde
Isel Fernandez <isel_77@hotmail.com>
2012-08-02 16:45:18 -0400

    168 - moving appfabric to a separate cookbook

--- cookbooks/appfabric/recipes/powershell.rb
+++ cookbooks/appfabric/recipes/powershell.rb
@@ -1,9 +1,7 @@
-ruby_scripts_dir = node['ruby_scripts_dir']
-
-template "#{ruby_scripts_dir}/appfabric_powershell.rb" do
-  source 'scripts/appfabric_powershell.erb'
+template "#{node['ruby_scripts_dir']}/appfabric_powershell.rb" do
+  source 'scripts/powershell.erb'
 end
 
 powershell "Deploy AppFabric powershell cmdlets" do
-  source("ruby #{ruby_scripts_dir}/appfabric_powershell.rb")
+  source("ruby #{node['ruby_scripts_dir']}/appfabric_powershell.rb")
 end
