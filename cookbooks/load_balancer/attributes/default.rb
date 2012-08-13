3462789cac34dab8cd1e336724389541d8554223
Isel Fernandez <isel_77@hotmail.com>
2012-08-13 15:36:47 -0400

    192 - increasing lb server timeout

--- cookbooks/load_balancer/attributes/default.rb
+++ cookbooks/load_balancer/attributes/default.rb
@@ -7,6 +7,6 @@ default[:app_server_ports] = '80,81,82'
 default[:health_check_uri] = '/HealthCheck.html'
 default[:maintenance_page] = '/system/maintenance.html'
 default[:max_connections_per_lb] = 255
-default[:server_timeout] = 50000
+default[:server_timeout] = 300000
 default[:session_stickiness] = 'false'
 default[:web_server_port] = '80'
