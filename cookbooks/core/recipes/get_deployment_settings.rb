34fa4febdd6ae1cce56c14986519681500988914
Isel Fernandez <isel_77@hotmail.com>
2012-08-15 14:50:13 -0400

    192 - fixing extra trailing comma

--- cookbooks/core/recipes/get_deployment_settings.rb
+++ cookbooks/core/recipes/get_deployment_settings.rb
@@ -2,7 +2,7 @@ template "#{node['ruby_scripts_dir']}/get_deployment_settings.rb" do
   source 'scripts/get_deployment_settings.erb'
   variables(
     :api_infrastructure_url => node['core']['api_infrastructure_url'],
-    :deployment_uri => node['core']['deployment_uri'],
+    :deployment_uri => node['core']['deployment_uri']
   )
 end
 
