79c354476d21c0dcfba0c2b939bc331dd6508219
Isel Fernandez <isel_77@hotmail.com>
2012-08-17 13:55:02 -0400

    192 - fixing tabbing in the file

--- cookbooks/deploy/recipes/event_router_service.rb
+++ cookbooks/deploy/recipes/event_router_service.rb
@@ -4,7 +4,6 @@ require 'yaml'
 
 ruby_scripts_dir = node['ruby_scripts_dir']
 
-
 target_directory = File.join(ENV['TEMP'], 'AppServer/Services/Messaging.EventRouter').gsub(/\\/, '/')
 install_path = File.join(ENV['ProgramData'], 'Windows Services\Messaging Event Router').gsub(/\\/, '/')
 installutil_command_fullpath= 'c:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe'
@@ -14,92 +13,80 @@ windows_service_change_completion_delay = 30
 service_assembly_filename=  'UltimateSoftware.Foundation.Messaging.EventRouter.exe'
 service_display_name = 'Ultimate Software Event Router Service'
 
-
 if File.exist?(target_directory) || File.exists?(install_path)
-
   # Cleanup block : Powershell
   # 1. issues a service stop request to Windows service
   # 2. uninstalls assembly from .Net 4.x 32 bit GAC
   # 3. confirms the target_directory and install_path no longer exist
-
   powershell 'Stopping,Uninstalling and removing Event Router Service' do
     parameters (
-                   {
-                       'DELAY' => windows_service_change_completion_delay,
-                       'INSTALL_PATH' => install_path.gsub('/', '\\'),
-                       'INSTALLUTIL_COMMAND_FULLPATH' => installutil_command_fullpath,
-                       'SERVICE_DISPLAY_NAME' => service_display_name,
-                       'SERVICE_ASSEMBLY_FILENAME' => service_assembly_filename,
-                       'SOURCE_PATH' => target_directory.gsub('/', '\\'),
-                   }
-               )
-
+      {
+        'DELAY' => windows_service_change_completion_delay,
+        'INSTALL_PATH' => install_path.gsub('/', '\\'),
+        'INSTALLUTIL_COMMAND_FULLPATH' => installutil_command_fullpath,
+        'SERVICE_DISPLAY_NAME' => service_display_name,
+        'SERVICE_ASSEMBLY_FILENAME' => service_assembly_filename,
+        'SOURCE_PATH' => target_directory.gsub('/', '\\'),
+      }
+    )
     powershell_script = <<-'EOF'
+      $assemblyFileName =  "$Env:SERVICE_ASSEMBLY_FILENAME"
+      $installPath = "$Env:INSTALL_PATH"
+      $installutil_command_fullpath = "$Env:INSTALLUTIL_COMMAND_FULLPATH"
+      $windowsServiceChangeCompletionDelay = $Env:DELAY
 
-$assemblyFileName =  "$Env:SERVICE_ASSEMBLY_FILENAME"
-$installPath = "$Env:INSTALL_PATH"
-$installutil_command_fullpath = "$Env:INSTALLUTIL_COMMAND_FULLPATH"
-$windowsServiceChangeCompletionDelay = $Env:DELAY
+      $serviceDisplayName = "$Env:SERVICE_DISPLAY_NAME"
 
-$serviceDisplayName = "$Env:SERVICE_DISPLAY_NAME"
+      $sourcePath = "$Env:SOURCE_PATH"
+      $uninstall_logFile = 'service_uninstall.log'
 
-$sourcePath = "$Env:SOURCE_PATH"
-$uninstall_logFile = 'service_uninstall.log'
+      Write-Output "Stop the service $serviceDisplayName"
+      cmd /c sc.exe stop """${serviceDisplayName}"""
 
-Write-Output "Stop the service $serviceDisplayName"
-cmd /c sc.exe stop """${serviceDisplayName}"""
+      Start-Sleep $windowsServiceChangeCompletionDelay
+      $Error.clear()
 
-Start-Sleep $windowsServiceChangeCompletionDelay
-$Error.clear()
+      # Repeat trhe last action
+      Write-Output "Stop the service ""$serviceDisplayName"""
+      cmd /c sc.exe stop """${serviceDisplayName}"""
+      $Error.clear()
 
-# Repeat trhe last action
-Write-Output "Stop the service ""$serviceDisplayName"""
-cmd /c sc.exe stop """${serviceDisplayName}"""
-$Error.clear()
+      Write-Output "Uninstall the service ""$serviceDisplayName""."
+      remove-item -path "${installPath}\${uninstall_logFile}" -ErrorAction SilentlyContinue
 
-Write-Output "Uninstall the service ""$serviceDisplayName""."
-remove-item -path "${installPath}\${uninstall_logFile}" -ErrorAction SilentlyContinue
+      chdir $installPath
 
+      Write-Output "cmd /c ${installutil_command_fullpath} ""/LogFile=${uninstall_logFile}"" /uninstall  ${assemblyFileName}"
+      cmd /c ${installutil_command_fullpath} "/LogFile=${uninstall_logFile}" /uninstall $assemblyFileName
 
+      Start-Sleep $windowsServiceChangeCompletionDelay
 
-chdir $installPath
+      Write-Output '------------------------'
+      Get-Content -Path $uninstall_logFile
+      Write-Output '------------------------'
 
-Write-Output "cmd /c ${installutil_command_fullpath} ""/LogFile=${uninstall_logFile}"" /uninstall  ${assemblyFileName}"
-cmd /c ${installutil_command_fullpath} "/LogFile=${uninstall_logFile}" /uninstall $assemblyFileName
+      chdir ${Env:TEMP}
+      if ((get-item -path "${installPath}") -ne $null) {
+        Write-Output "Removing directory ""${installPath}"""
+        Remove-Item -Path "${installPath}" -Recurse  -Force
+        Write-Output "directory ""${installPath}"" should be empty"
+        Get-ChildItem -path ${installPath} -ErrorAction SilentlyContinue
+      }
 
-Start-Sleep $windowsServiceChangeCompletionDelay
+      if ((get-item -path "${sourcePath}") -ne $null) {
+        Write-Output "Removing directory ""${sourcePath}"""
+        Remove-Item -Path "${sourcePath}" -Recurse -Force
+        Write-Output "directory ""${sourcePath}"" should be empty"
+        Get-ChildItem -path "${sourcePath}" -ErrorAction SilentlyContinue
+      }
 
-Write-Output '------------------------'
-Get-Content -Path $uninstall_logFile
-Write-Output '------------------------'
+      # TODO - inspect the assembly is no longer in the GAC
+      # Note - gacutil.exe is no longer part of .NET redistributable
+      # http://stackoverflow.com/questions/2660355/net-4-0-has-a-new-gac-why
 
-chdir ${Env:TEMP}
-if ((get-item -path "${installPath}") -ne $null) {
-Write-Output "Removing directory ""${installPath}"""
-Remove-Item -Path "${installPath}" -Recurse  -Force
-Write-Output "directory ""${installPath}"" should be empty"
-Get-ChildItem -path ${installPath} -ErrorAction SilentlyContinue
-
-}
-
-if ((get-item -path "${sourcePath}") -ne $null) {
-
-Write-Output "Removing directory ""${sourcePath}"""
-Remove-Item -Path "${sourcePath}" -Recurse -Force
-Write-Output "directory ""${sourcePath}"" should be empty"
-Get-ChildItem -path "${sourcePath}" -ErrorAction SilentlyContinue
-
-}
-
-# TODO - inspect the assembly is no longer in the GAC
-# Note - gacutil.exe is no longer part of .NET redistributable
-# http://stackoverflow.com/questions/2660355/net-4-0-has-a-new-gac-why
-
-$Error.clear()
-
+      $Error.clear()
     EOF
     source(powershell_script)
-
   end
 else
   puts 'Event Router Service was not installed on the system'
@@ -108,17 +95,13 @@ end
 puts "Copying Event Router Service to #{target_directory} and updating configurations"
 
 template "#{ruby_scripts_dir}/event_router_service.rb" do
-
-# Copy build artifacts from DeployBinaries and
-# configure service settings through "change_app_setting" library call - ruby
-
   source 'scripts/event_router_service.erb'
   variables(
-      :binaries_directory => node[:binaries_directory],
-      :db_server => node[:deploy][:db_server],
-      :messaging_server => node[:deploy][:messaging_server],
-      :source_directory => File.join(node[:binaries_directory], 'AppServer/Services/Messaging.EventRouter').gsub(/\\/, '/'),
-      :target_directory => target_directory
+    :binaries_directory => node[:binaries_directory],
+    :db_server => node[:deploy][:db_server],
+    :messaging_server => node[:deploy][:messaging_server],
+    :source_directory => File.join(node[:binaries_directory], 'AppServer/Services/Messaging.EventRouter').gsub(/\\/, '/'),
+    :target_directory => target_directory
   )
 end
 
@@ -129,24 +112,20 @@ end
 # Install block : powershell
 # Installs assembly into .Net 4.x 32 bit GAC
 # issues start command to Windows service
-
 powershell 'Install Event Router Service' do
   parameters (
-                 {
-                     'DELAY' => windows_service_change_completion_delay,
-                     'INSTALL_PATH' => install_path.gsub('/', '\\'),
-                     'INSTALLUTIL_COMMAND_FULLPATH' => installutil_command_fullpath,
-                     'SERVICE_ASSEMBLY_FILENAME' => service_assembly_filename,
-                     'SERVICE_DISPLAY_NAME' => service_display_name,
-                     'SERVICE_PORT' => node[:event_router_port],
-                     'SERVER_MANAGER_FEATURES' => node[:msmq_features],
-                     'SOURCE_PATH' => target_directory.gsub('/', '\\')
-                 }
-             )
-
+    {
+      'DELAY' => windows_service_change_completion_delay,
+      'INSTALL_PATH' => install_path.gsub('/', '\\'),
+      'INSTALLUTIL_COMMAND_FULLPATH' => installutil_command_fullpath,
+      'SERVICE_ASSEMBLY_FILENAME' => service_assembly_filename,
+      'SERVICE_DISPLAY_NAME' => service_display_name,
+      'SERVICE_PORT' => node[:event_router_port],
+      'SERVER_MANAGER_FEATURES' => node[:msmq_features],
+      'SOURCE_PATH' => target_directory.gsub('/', '\\')
+    }
+  )
   powershell_script = <<-'EOF'
-
-
 $serviceDisplayName = "$Env:SERVICE_DISPLAY_NAME"
 
 write-output "REBOOT=${Env:RS_REBOOT}"
