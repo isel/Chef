1ca755e327ab75145d1393dfbb9247eaf0c4a72c
Isel Fernandez <isel_77@hotmail.com>
2012-08-17 11:41:57 -0400

    192 - getting ports from settings service instead of cookbook default values

--- cookbooks/smoke_tests/attributes/default.rb
+++ cookbooks/smoke_tests/attributes/default.rb
@@ -2,8 +2,3 @@ default['ruby_scripts_dir'] = '/RubyScripts'
 default['binaries_directory'] = '/DeployScripts_Binaries'
 
 default[:activemq_console_port] = '8161'
-default[:activemq_port] = '61616'
-default[:engine_port] = '8080'
-default[:messaging_port] = '8081'
-default[:mule_port] = '8585'
-default[:search_port] = '9200'
--- cookbooks/smoke_tests/recipes/local_search.rb
+++ cookbooks/smoke_tests/recipes/local_search.rb
@@ -1,7 +1,6 @@
 template "#{node['ruby_scripts_dir']}/local_search.rb" do
   source 'scripts/local_search.erb'
   variables(
-    :search_port => node[:search_port],
     :server_type => node[:core][:server_type]
   )
 end
