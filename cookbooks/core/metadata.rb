b0b8f2e0316f92d0cf2bb02130378dae0d38e8eb
Ronald Ocampo <ronald_ocampo@ultimatesoftware.com>
2012-08-30 17:46:07 -0400

    214 - Adding recipe to install jre for windows

--- cookbooks/core/metadata.rb
+++ cookbooks/core/metadata.rb
@@ -10,6 +10,7 @@ supports "ubuntu"
 recipe "core::download_vendor_artifacts_prereqs", "Sets up prereqs for downloading vendor artifacts"
 recipe "core::get_deployment_settings", "Gets the deployment settings from the services api"
 recipe "core::install_gems", "Installs ruby gems"
+recipe "core::install_jre", "Installs jre for windows and ubuntu"
 recipe "core::install_ruby", "Installs ruby from source"
 recipe "core::netsh_advfirewall_management", "Disables Windows Firewall"
 recipe "core::set_rightscale_account", "sets the Rightscale account"
@@ -25,12 +26,12 @@ attribute "core/api_infrastructure_url",
 attribute "core/aws_access_key_id",
   :display_name => "aws access key id",
   :required => "required",
-  :recipes => ["core::install_ruby"]
+  :recipes => ["core::install_ruby", "core::install_jre"]
 
 attribute "core/aws_secret_access_key",
   :display_name => "aws secret access key",
   :required => "required",
-  :recipes => ["core::install_ruby"]
+  :recipes => ["core::install_ruby", "core::install_jre"]
 
 attribute "core/deployment_uri",
   :display_name => "deployment uri",
@@ -48,4 +49,4 @@ attribute "core/s3_bucket",
   :description => "i.e. ugfartifacts, ugfproduction",
   :required => "optional",
   :default  => "ugfartifacts",
-  :recipes => ["core::install_ruby"]
+  :recipes => ["core::install_ruby", "core::install_jre"]
---
+++ cookbooks/core/recipes/install_jre.rb
@@ -0,0 +1,78 @@
+include_recipe 'core::download_vendor_artifacts_prereqs'
+
+if node[:platform] == "ubuntu"
+#  if File.exists?('/usr/local/mongodb')
+#    log 'Mongo already downloaded.'
+#  else
+#    version = node[:deploy][:mongo_version]
+#
+#    template "#{node['ruby_scripts_dir']}/download_mongo.rb" do
+#      local true
+#      source "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
+#      variables(
+#        :aws_access_key_id => node[:core][:aws_access_key_id],
+#          :aws_secret_access_key => node[:core][:aws_secret_access_key],
+#          :s3_bucket => node[:core][:s3_bucket],
+#          :s3_repository => 'Vendor',
+#          :product => 'mongo',
+#          :version => version,
+#          :artifacts => 'mongo',
+#          :target_directory => '/usr/local',
+#          :unzip => true
+#      )
+#    end
+#
+#    bash 'Downloading mongo' do
+#      code <<-EOF
+#      ruby #{node['ruby_scripts_dir']}/download_mongo.rb
+#      mv /usr/local/mongo /usr/local/mongodb
+#      chmod a+x /usr/local/mongodb/bin/*
+#      EOF
+#    end
+#  end
+#  bash 'Installing ruby gems' do
+#    code <<-EOF
+#apt-get install -y libyaml-dev
+#apt-cache policy libyaml-dev
+#
+#gem install psych -v 1.3.2 --no-rdoc --no-ri
+#
+#gem update --system
+#
+##{gems.map {|g| "gem install #{g['gem']} -v #{g['version']} --no-rdoc --no-ri \n"}.join}
+#    EOF
+#  end
+else
+  template "#{node['ruby_scripts_dir']}/download_jre.rb" do
+    local true
+    source "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
+    variables(
+      :aws_access_key_id => node[:core][:aws_access_key_id],
+      :aws_secret_access_key => node[:core][:aws_secret_access_key],
+      :s3_bucket => node[:core][:s3_bucket],
+      :s3_repository => 'Vendor',
+      :product => 'jre',
+      :version => '1.7.0',
+      :artifacts => 'jre_windows',
+      :target_directory => '/download_jre',
+      :unzip => true
+    )
+  end
+
+  powershell 'Installing jre' do
+    script = <<-EOF
+  cd download_jre
+
+  if (Test-Path("c:\jre")) {
+  Write-Output "JRE already installed"
+  Exit 0
+  }
+
+  cmd /c 'msiexec.exe /i jre1.7.0.msi /qn INSTALLDIR=c:\jre'
+
+  [System.Environment]::SetEnvironmentVariable('JAVA_HOME', 'c:\jre\bin', 'machine')
+  [System.Environment]::SetEnvironmentVariable('JRE_HOME', 'c:\jre\bin', 'machine')
+    EOF
+    source(script)
+  end
+end
