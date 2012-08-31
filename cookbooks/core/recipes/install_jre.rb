6637fb4a3a7ad9bc38c7b70338f1bc7a44bad617
Ronald Ocampo <ronald_ocampo@ultimatesoftware.com>
2012-08-31 14:26:17 -0400

    214 - Modifying jre install for windows

--- cookbooks/core/recipes/install_jre.rb
+++ cookbooks/core/recipes/install_jre.rb
@@ -1,47 +1,33 @@
 include_recipe 'core::download_vendor_artifacts_prereqs'
 
 if node[:platform] == "ubuntu"
-#  if File.exists?('/usr/local/mongodb')
-#    log 'Mongo already downloaded.'
-#  else
-#    version = node[:deploy][:mongo_version]
-#
-#    template "#{node['ruby_scripts_dir']}/download_mongo.rb" do
-#      local true
-#      source "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
-#      variables(
-#        :aws_access_key_id => node[:core][:aws_access_key_id],
-#          :aws_secret_access_key => node[:core][:aws_secret_access_key],
-#          :s3_bucket => node[:core][:s3_bucket],
-#          :s3_repository => 'Vendor',
-#          :product => 'mongo',
-#          :version => version,
-#          :artifacts => 'mongo',
-#          :target_directory => '/usr/local',
-#          :unzip => true
-#      )
-#    end
-#
-#    bash 'Downloading mongo' do
-#      code <<-EOF
-#      ruby #{node['ruby_scripts_dir']}/download_mongo.rb
-#      mv /usr/local/mongo /usr/local/mongodb
-#      chmod a+x /usr/local/mongodb/bin/*
-#      EOF
-#    end
-#  end
-#  bash 'Installing ruby gems' do
-#    code <<-EOF
-#apt-get install -y libyaml-dev
-#apt-cache policy libyaml-dev
-#
-#gem install psych -v 1.3.2 --no-rdoc --no-ri
-#
-#gem update --system
-#
-##{gems.map {|g| "gem install #{g['gem']} -v #{g['version']} --no-rdoc --no-ri \n"}.join}
-#    EOF
-#  end
+  #template "#{node['ruby_scripts_dir']}/download_jre.rb" do
+  #  local true
+  #  source "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
+  #  variables(
+  #    :aws_access_key_id => node[:core][:aws_access_key_id],
+  #    :aws_secret_access_key => node[:core][:aws_secret_access_key],
+  #    :s3_bucket => node[:core][:s3_bucket],
+  #    :s3_repository => 'Vendor',
+  #    :product => 'jre',
+  #    :version => '1.7.0',
+  #    :artifacts => 'jre_ubuntu',
+  #    :target_directory => '/',
+  #    :unzip => true
+  #  )
+  #end
+  #
+  #bash 'Installing jre' do
+  #  code <<-EOF
+  #  if grep JAVA_HOME /etc/profile; then
+  #    echo "JRE already installed"
+  #  else
+  #    ruby #{node['ruby_scripts_dir']}/download_jre.rb
+  #    echo "JAVA_HOME=/usr/lib/jvm/java-6-sun" >> /etc/profile
+  #    echo "PATH=\$PATH:\$JAVA_HOME/bin" >> /etc/profile
+  #  fi
+  #  EOF
+  #end
 else
   template "#{node['ruby_scripts_dir']}/download_jre.rb" do
     local true
@@ -57,25 +43,20 @@ else
       :target_directory => '/download_jre',
       :unzip => true
     )
+    not_if { File.exist?('/jre') }
   end
 
   powershell 'Installing jre' do
     script = <<-EOF
       ruby #{node['ruby_scripts_dir']}/download_jre.rb
       cd /download_jre/jre_windows
-Write-Output "before if"
-      if (Test-Path("c:\jre")) {
-        Write-Output "JRE already installed"
-        Exit 0
-      }
-Write-Output "before msiexec"
 
-      cmd /c 'msiexec.exe /i jre1.7.0.msi /qn INSTALLDIR=c:\jre'
-Write-Output "before env vars"
+      cmd /c 'msiexec.exe /i jre1.7.0.msi /qn INSTALLDIR=c:\\jre'
 
-      [System.Environment]::SetEnvironmentVariable('JAVA_HOME', 'c:\jre\bin', 'machine')
-      [System.Environment]::SetEnvironmentVariable('JRE_HOME', 'c:\jre\bin', 'machine')
+      [System.Environment]::SetEnvironmentVariable('JAVA_HOME', 'c:\\jre\\bin', 'machine')
+      [System.Environment]::SetEnvironmentVariable('JRE_HOME', 'c:\\jre\\bin', 'machine')
     EOF
     source(script)
+    not_if { File.exist?('/jre') }
   end
 end
