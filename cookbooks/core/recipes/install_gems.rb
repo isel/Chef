f6823544a8c2b8124337b027fdc6f0ecef35be78
Robert Rehberg <robertrehberg@bellsouth.net>
2012-10-10 11:41:17 -0400

    AZ-260: Making gem install idempotent

--- cookbooks/core/recipes/install_gems.rb
+++ cookbooks/core/recipes/install_gems.rb
@@ -15,7 +15,7 @@ class Chef::Recipe
   include LocalGems
 end
 
-puts gems_to_install(gems)
+new_gems = gems_to_install(gems)
 
 if node[:platform] == "ubuntu"
   bash 'Installing ruby gems' do
@@ -27,14 +27,14 @@ gem install psych -v 1.3.2 --no-rdoc --no-ri
 
 gem update --system
 
-#{gems_to_install(gems).map { |gem, version| "gem install #{gem} -v #{version} --no-rdoc --no-ri \n" }.join}
+#{new_gems.map { |gem, version| "gem install #{gem} -v #{version} --no-rdoc --no-ri \n" }.join}
     EOF
   end
 else
   powershell 'Installing ruby gems' do
     script = <<-EOF
 & "gem" 'update' '--system'
-#{gems_to_install(gems).map { |gem, version| "& 'gem' 'install' '#{gem}' -v '#{version}' '--no-rdoc' '--no-ri' \n" }.join}
+#{new_gems.map { |gem, version| "& 'gem' 'install' '#{gem}' -v '#{version}' '--no-rdoc' '--no-ri' \n" }.join}
     EOF
     source(script)
   end
