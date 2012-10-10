179ebfab6b0f343b6bb599951adff871f28ad741
Robert Rehberg <robertrehberg@bellsouth.net>
2012-10-10 13:14:32 -0400

    Adding a new cache (Configuration)

--- cookbooks/appfabric/attributes/default.rb
+++ cookbooks/appfabric/attributes/default.rb
@@ -6,7 +6,7 @@ default[:pims_directory] = '/DeployScripts_PIMs'
 default[:powershell_x32_dir] = '/Windows/system32/WindowsPowerShell/v1.0'
 default[:powershell_x64_dir] = '/Windows/sysnative/WindowsPowerShell/v1.0'
 
-default[:caches] = 'default,TokenStore,SaasPolicy,EntityModel,Securables,Messages,Views,Enumerations,BusinessProcess'
+default[:caches] = 'default,TokenStore,SaasPolicy,EntityModel,Securables,Messages,Views,Enumerations,BusinessProcess,Configuration'
 default[:service_user] = 'appfabric'
 default[:shared_drive] = 'appfabric_caching'
 default[:shared_folder] = "c:\\appfabric_caching"
