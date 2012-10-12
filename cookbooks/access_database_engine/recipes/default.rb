b9c4f7001af95da11f290d87e17352bde9adebdb
Robert Rehberg <robertrehberg@bellsouth.net>
2012-10-12 15:01:27 -0400

    AZ-260: Extracting the script attachments to our S3 bucket

---
+++ cookbooks/access_database_engine/recipes/default.rb
@@ -0,0 +1,28 @@
+include_recipe 'core::download_vendor_artifacts_prereqs'
+
+template "#{node['ruby_scripts_dir']}/download_access_database_engine.rb" do
+  local true
+  source "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
+  variables(
+    :aws_access_key_id => node[:core][:aws_access_key_id],
+    :aws_secret_access_key => node[:core][:aws_secret_access_key],
+    :s3_bucket => node[:core][:s3_bucket],
+    :s3_repository => 'Vendor',
+    :product => 'accessdatabaseengine',
+    :version => '14.0',
+    :artifacts => 'accessdatabaseengine',
+    :target_directory => '/installs',
+    :unzip => true
+  )
+  not_if { File.exist?('/installs/accessdatabaseengine.zip') }
+end
+
+powershell 'Download Access Database Engine' do
+  source("ruby #{node['ruby_scripts_dir']}/download_access_database_engine.rb")
+  not_if { File.exist?('/installs/accessdatabaseengine.zip') }
+end
+
+powershell 'Install Access Database Engine' do
+  source('cmd /c "/installs/AccessDatabaseEngine_x64.exe /log:c:\accessdbengine.log /quiet /norestart"')
+  not_if { File.exist?('/accessdbengine.log') }
+end
--- cookbooks/appfabric/recipes/install.rb
+++ cookbooks/appfabric/recipes/install.rb
@@ -1,3 +1,56 @@
+include_recipe 'core::download_vendor_artifacts_prereqs'
+
+template('/installs/set_password_to_not_expire.bat') { source 'set_password_to_not_expire.erb' }
+
+template "#{node['ruby_scripts_dir']}/download_appfabric.rb" do
+  local true
+  source "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
+  variables(
+    :aws_access_key_id => node[:core][:aws_access_key_id],
+    :aws_secret_access_key => node[:core][:aws_secret_access_key],
+    :s3_bucket => node[:core][:s3_bucket],
+    :s3_repository => 'Vendor',
+    :product => 'appfabric',
+    :version => '6.1',
+    :artifacts => 'appfabric',
+    :target_directory => '/installs',
+    :unzip => true
+  )
+  not_if { File.exist?('/installs/WindowsServerAppFabricSetup_x64_6.1.exe') }
+end
+
+powershell 'Download AppFabric' do
+  source("ruby #{node['ruby_scripts_dir']}/download_appfabric.rb")
+  not_if { File.exist?('/installs/WindowsServerAppFabricSetup_x64_6.1.exe') }
+end
+
+template "#{node['ruby_scripts_dir']}/download_appfabric_admin_tool.rb" do
+  local true
+  source "#{node['ruby_scripts_dir']}/download_vendor_artifacts.erb"
+  variables(
+    :aws_access_key_id => node[:core][:aws_access_key_id],
+    :aws_secret_access_key => node[:core][:aws_secret_access_key],
+    :s3_bucket => node[:core][:s3_bucket],
+    :s3_repository => 'Vendor',
+    :product => 'appfabric',
+    :version => '6.1',
+    :artifacts => 'appfabricadmintool',
+    :target_directory => '/appfabric_caching_admin',
+    :unzip => true
+  )
+  not_if { File.exist?('/appfabric_caching_admin') }
+end
+
+powershell 'Download AppFabric' do
+  source("ruby #{node['ruby_scripts_dir']}/download_appfabric.rb")
+  not_if { File.exist?('/installs/WindowsServerAppFabricSetup_x64_6.1.exe') }
+end
+
+powershell 'Download AppFabric Admin Tool' do
+  source("ruby #{node['ruby_scripts_dir']}/download_appfabric_admin_tool.rb")
+  not_if { File.exist?('/appfabric_caching_admin') }
+end
+
 powershell "Install AppFabric" do
   powershell_script = <<'POWERSHELL_SCRIPT'
     if (Test-Path "$env:windir\system32\AppFabric")
