f17936ea8fba689ca8bdaeda1e5925ad43011f5a
Isel Fernandez <isel_77@hotmail.com>
2012-10-12 13:48:34 -0400

    260 - fixing log entry for get_deployment_settings

--- cookbooks/core/recipes/get_deployment_settings.rb
+++ cookbooks/core/recipes/get_deployment_settings.rb
@@ -1,4 +1,6 @@
-template "#{node['ruby_scripts_dir']}/get_deployment_settings.rb" do
+script = "#{node['ruby_scripts_dir']}/get_deployment_settings.rb"
+
+template script do
   source 'scripts/get_deployment_settings.erb'
   variables(
     :api_infrastructure_url => node['core']['api_infrastructure_url'],
@@ -7,14 +9,8 @@ template "#{node['ruby_scripts_dir']}/get_deployment_settings.rb" do
 end
 
 if node[:platform] == "ubuntu"
-  bash 'Downloading artifacts' do
-    code <<-EOF
-      ruby #{node['ruby_scripts_dir']}/get_deployment_settings.rb
-    EOF
-  end
+  bash('Getting deployment settings') { code "ruby #{script}" }
 else
-  powershell "Downloading artifacts" do
-    source("ruby #{node['ruby_scripts_dir']}/get_deployment_settings.rb")
-  end
+  powershell('Getting deployment settings') { source("ruby #{script}") }
 end
 
