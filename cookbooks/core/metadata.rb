6ca075157012467252c8de7d94f488da806a3871
Isel Fernandez <isel_77@hotmail.com>
2012-10-15 15:48:31 -0400

    260 - moving all ruby recipes to the ruby cookbook

--- cookbooks/core/metadata.rb
+++ cookbooks/core/metadata.rb
@@ -10,7 +10,6 @@ supports "ubuntu"
 recipe "core::download_vendor_artifacts_prereqs", "Sets up prereqs for downloading vendor artifacts"
 recipe "core::get_deployment_settings", "Gets the deployment settings from the services api"
 recipe "core::install_jdk", "Installs jdk for windows and ubuntu"
-recipe "core::install_ruby", "Installs ruby from source"
 recipe "core::netsh_advfirewall_management", "Disables Windows Firewall"
 recipe "core::set_rightscale_account", "sets the Rightscale account"
 recipe "core::setup_powershell_runtimes", "Allows up the poweshell to run multiple runtimes"
@@ -25,12 +24,12 @@ attribute "core/api_infrastructure_url",
 attribute "core/aws_access_key_id",
   :display_name => "aws access key id",
   :required => "required",
-  :recipes => ["core::install_ruby", "core::install_jdk"]
+  :recipes => ["core::install_jdk"]
 
 attribute "core/aws_secret_access_key",
   :display_name => "aws secret access key",
   :required => "required",
-  :recipes => ["core::install_ruby", "core::install_jdk"]
+  :recipes => ["core::install_jdk"]
 
 attribute "core/deployment_uri",
   :display_name => "deployment uri",
@@ -48,4 +47,4 @@ attribute "core/s3_bucket",
   :description => "i.e. ugfartifacts, ugfproduction",
   :required => "optional",
   :default  => "ugfgate1",
-  :recipes => ["core::install_ruby", "core::install_jdk"]
+  :recipes => ["core::install_jdk"]
