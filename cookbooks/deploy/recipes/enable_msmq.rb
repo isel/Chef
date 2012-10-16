b2857dd02a8f2c158f3d7fd277fc379f894dd588
Robert Rehberg <robertrehberg@bellsouth.net>
2012-10-16 13:37:08 -0400

    AZ-260: Speeding up msmq reboots

--- cookbooks/deploy/recipes/enable_msmq.rb
+++ cookbooks/deploy/recipes/enable_msmq.rb
@@ -2,11 +2,10 @@
 powershell 'Enable MSMQ' do
   parameters ({ 'SERVER_MANAGER_FEATURES' => node[:msmq_features] })
   powershell_script = <<-'EOF'
-   # the script install components in Microsoft recommended way
-   # that is only available on  Windows 2008 R2
-   # TODO - merge with legacy way (servermanagercmd.exe)
+# as long as this is a constant, we can break on the first discovery of an installed component
+# also try removing the additionalinfo check and see if it is costing us time
+# move recipe below installing ruby gems
 
-   # http://blogs.msdn.com/b/johnbreakwell/archive/2007/06/19/minimalist-setup-script-for-msmq-unattended-installation.aspx
     Import-Module ServerManager
     $features_array = $Env:SERVER_MANAGER_FEATURES -split ','
 
@@ -15,11 +14,10 @@ powershell 'Enable MSMQ' do
       $check = Get-WindowsFeature -name $feature
       if ($check -ne $null  ) {
         if ($check.Installed -ne $True ) {
-          write-output  "Installing Feature ${feature}"
+          write-output  "Installing feature ${feature}"
           Add-WindowsFeature -name $feature
         } else {
-            write-output "Feature ${feature} Already installed "
-            write-output $check.AdditionalInfo  " "  $check.Installed
+            write-output "Feature ${feature} already installed "
         }
       } else {
           write-output  "Feature ${feature} is unknown to Windows Server Manager"
