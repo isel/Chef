e976787c221742f04b0baf5e6b3a5ebe0fe971ac
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-10-16 13:43:17 -0400

    253 - Add JSPR functional tests into the pipeline  - merged to copy the 'set_ruby_path.rb'

--- cookbooks/teamcity/recipes/update_buildagent_configuration.rb
+++ cookbooks/teamcity/recipes/update_buildagent_configuration.rb
@@ -1,38 +1,10 @@
-require 'rake'
-require 'fileutils'
-
 powershell 'Update TC configuration' do
-  parameters()
-powershell_script = <<-'EOF'
-write-output "Probing RS_REBOOT=${env:RS_REBOOT}"
-
-if ( (${Env:RS_REBOOT} -ne $null) -and (${Env:RS_REBOOT} -match 'true'))  {
-   write-output 'Skipping TC configuration change during reboot.'
-   $Error.Clear()
-   exit 0
-   }
-
-$build_agent_home ='C:\BuildAgent'
-$properties_file = "${build_agent_home}\\conf\\buildAgent.properties"
-$setting_value = 'UTF-8'
-$setting_name = 'system.file.encoding'
-
-$properties = Get-Content $properties_file
-if ($properties -match "^${setting_name}=") {
-  Write-output "Updating setting ${setting_name}"
-  $properties =  $properties -Replace  "^${setting_name}=.+$", "${setting_name}=${setting_value}"
-} else {
-  Write-output "Creating setting ${setting_name}"
-  $properties += @"
-${setting_name}=${setting_value}
-"@
-}
-
-Set-Content  -literalPath $properties_file -value $properties
-
-$Error.clear()
-  EOF
-  source(powershell_script)
+  parameters (
+    {
+        'properties_file' => node['properties_file'],
+        'key' => 'system.file.encoding',
+        'value' => 'UTF-8'
+    }
+  )
+  source(node['set_value_in_properties_file_powershell_script'])
 end
-
-
