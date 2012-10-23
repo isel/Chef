3dff6484fa9a77a9d7149a8fbae5aee9184b3267
Isel Fernandez <isel_77@hotmail.com>
2012-10-23 10:52:00 -0400

    219 - using plugin directory on provisioning

--- cookbooks/deploy/attributes/default.rb
+++ cookbooks/deploy/attributes/default.rb
@@ -1,6 +1,7 @@
 default[:ruby_scripts_dir] = '/RubyScripts'
 default[:binaries_directory] = '/DeployScripts_Binaries'
 default[:infrastructure_directory] = '/DeployScripts_Infrastructure'
+default[:plugins_directory] = '/DeployScripts_Plugins'
 default[:pims_directory] = '/DeployScripts_PIMs'
 default[:deployment_settings_json] = "#{default[:ruby_scripts_dir]}/deployment_settings.json"
 default[:deployment_settings_xml] = "#{default[:ruby_scripts_dir]}/deployment_settings.xml"
