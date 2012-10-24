e9efa125be973223d4bda2deb6fe6923d23c22a4
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-10-24 09:57:04 -0400

    278 - Remove start-sleep cycles from the event_router recipe - misc unneeded logging cleanup

--- cookbooks/deploy/recipes/event_router_service.rb
+++ cookbooks/deploy/recipes/event_router_service.rb
@@ -14,13 +14,13 @@ template "#{ruby_scripts_dir}/event_router_service.rb" do
       :db_server => node[:deploy][:db_server],
       :install_directory => File.join(ENV['ProgramData'], 'Windows Services\Messaging Event Router').gsub(/\\/, '/'),
       :messaging_server => node[:deploy][:messaging_server],
-      :service_display_name => 'Ultimate Software Event Router Service',
       :service_assembly_filename => 'UltimateSoftware.Foundation.Messaging.EventRouter.exe',
-      :source_directory => File.join(node[:binaries_directory], 'AppServer/Services/Messaging.EventRouter').gsub(/\\/, '/'),
-      :staging_directory => File.join(ENV['TEMP'], 'AppServer/Services/Messaging.EventRouter').gsub(/\\/, '/'),
       :service_change_timeout => 30,
       :service_launch_timeout => 300,
-      :service_query_timeout => 5
+      :service_query_timeout => 5,
+      :service_display_name => 'Ultimate Software Event Router Service',
+      :source_directory => File.join(node[:binaries_directory], 'AppServer/Services/Messaging.EventRouter').gsub(/\\/, '/'),
+      :staging_directory => File.join(ENV['TEMP'], 'AppServer/Services/Messaging.EventRouter').gsub(/\\/, '/')
     }
   )
 end
