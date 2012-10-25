9e8f441b48d59374012fd844e44f8d33177e8ef4
RonaldO <ronald_ocampo@ultimatesoftware.com>
2012-10-25 17:03:53 -0400

    261 - Adding recipe to add admin user for replica sets after replica set is initialized

--- cookbooks/deploy/recipes/add_admin_replica_set.rb
+++ cookbooks/deploy/recipes/add_admin_replica_set.rb
@@ -1,6 +1,15 @@
+template "#{node[:ruby_scripts_dir]}/add_admin_replica_set.rb" do
+  source 'scripts/add_admin_replica_set.erb'
+  variables(
+      :admin_password_mongo => node[:deploy][:admin_password_mongo],
+      :admin_user_mongo => node[:deploy][:admin_user_mongo]
+  )
+  only_if { node[:deploy][:is_primary_db] == 'true' && node[:deploy][:use_replication] == 'true' }
+end
+
 bash 'Add admin user for mongo with replica set' do
   code <<-EOF
-    mongo admin --eval "db.addUser(\"#{node[:deploy][:admin_user_mongo]}\",\"#{node[:deploy][:admin_password_mongo]}\")"
+    ruby #{node[:ruby_scripts_dir]}/add_admin_replica_set.rb
   EOF
   only_if { node[:deploy][:is_primary_db] == 'true' && node[:deploy][:use_replication] == 'true' }
 end
