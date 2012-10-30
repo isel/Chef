8057a120eca1dbc971c46895cc49b9c9f94db463
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-10-30 15:52:00 -0400

    291 install  mongod on windows integration build agents - conversion to Ruby

--- cookbooks/mongo/attributes/default.rb
+++ cookbooks/mongo/attributes/default.rb
@@ -1,2 +1,3 @@
 default[:ruby_scripts_dir] = '/RubyScripts'
+default[:binaries_directory] = '/DeployScripts_Binaries'
 default[:deployment_settings_json] = "#{default[:ruby_scripts_dir]}/deployment_settings.json"
--- cookbooks/mongo/recipes/default.rb
+++ cookbooks/mongo/recipes/default.rb
@@ -34,89 +34,28 @@ if node[:platform] == 'ubuntu'
     not_if { File.exist?(install_directory) }
   end
 else
+
   # settings = JSON.parse(File.read(node['deployment_settings_json']))
   # database_port = settings['database_port']
   database_port = '27017'
-  install_directory_windows = install_directory.gsub(/\//, '\\\\')
+  ruby_scripts_dir =  node[:ruby_scripts_dir]
+
+  template "#{ruby_scripts_dir}/install_mongo.rb" do
+    local true
+
+    source "scripts/install_mongo.erb"
+    variables(
+      :binaries_directory => node[:binaries_directory],
+      :install_directory => install_directory,
+      :service_name => 'mongoDB',
+      :target_directory => target_directory,
+      :timeout => 30
+    )
+    not_if { File.exist?(install_directory) }
+  end
 
   powershell 'Installing mongo' do
-
-    script = <<-EOF
-
-      ruby #{node[:ruby_scripts_dir]}/download_mongo.rb         -
-
-      $erroractionpreference = 'SilentlyContinue'
-      $ServiceName = 'MongoDB'
-
-      new-item -path "#{install_directory}" -Type Directory -Force -ErrorAction SilentlyContinue | out-null
-
-      cd #{install_directory}
-
-      copy-item "#{target_directory}/mongo_windows/mongo/*" -destination . -recurse -Force
-
-      # create logpath
-      new-item -path log -Type Directory -Force | out-null
-
-      # create datapath
-      new-item -path data/db  -Type Directory -Force | out-null
-
-      # update configuration
-      $conf = '#{install_directory_windows}\\mongod.conf'
-      (Get-Content ($conf)) | Foreach-Object {$_ -replace "^port +=.+$", ("port = " + #{database_port})} | Set-Content  ($conf)
-
-      # Install the MongoDB Service
-      bin\\mongod.exe --config $conf --install  --rest
-
-      $ServiceKey = "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\${ServiceName}"
-      write-output "Reading registry key of the service`n" ,   $ServiceKey
-
-      # Run the MongoDB Service
-      sc.exe start $ServiceName
-
-      # Query the service
-      sc.exe query $ServiceName
-
-      $Error.clear()
-
-      # busy wait loop  for mogo.exe shell to be able to process commands
-      $Env:MONGO_HOME = '#{install_directory_windows}'
-      $Env:Path = "${Env:PATH};${Env:MONGO_HOME}\\bin"
-
-
-      $command = "mongo.exe --eval ""quit();"""
-      $finished = false
-      $Env:TIMEOUT = 100
-      $cumulative_wait_time = 0
-      $wait_time = 10
-      $action = 'connect to mongo'
-      Write-output "Trying ${action}"
-
-      while ($cumulative_wait_time -lt ${Env:TIMEOUT}){
-
-        $response = invoke-command  {cmd /c $command }
-        $Error.clear()
-        write-host $response
-        if ($response -match 'Error:'){
-          $cumulative_wait_time = $cumulative_wait_time + $wait_time
-          write-host "Could not ${action} for ${cumulative_wait_time} sec. Retry after $wait_time sec."
-          start-sleep  $wait_time
-        }
-        else {
-          $finished = $true
-          break
-        }
-      }
-
-      if (!$finished){
-        Write-Host "Could not ${action}"
-       Exit 1
-      }
-
-      Write-host "Done ${action}"
-      $Error.clear()
-
-    EOF
-    source(script)
+    source("ruby #{ruby_scripts_dir}/foundation_services.rb")
     not_if { File.exist?(install_directory) }
   end
 end
