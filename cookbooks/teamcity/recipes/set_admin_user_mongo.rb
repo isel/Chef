57c1ce28c8530ed1ad425c5d7bea794f1533f009
serguei kouzmine <serguei_kouzmine@ultimatesoftware.com>
2012-10-30 11:00:48 -0400

    291 install  mongod on windows integration build agents -follow TC naming conventions in environment variables

--- cookbooks/teamcity/recipes/set_admin_user_mongo.rb
+++ cookbooks/teamcity/recipes/set_admin_user_mongo.rb
@@ -1,9 +1,9 @@
 powershell "Set administrator user for mongo" do
   parameters (
     {
-       :properties_file => node[:properties_file],
-       'key' => 'env.AdminUserMongo',
-       'value' => node[:teamcity][:admin_user_mongo],
+      :properties_file => node[:properties_file],
+      'key' => 'env.db.user',
+      'value' => node[:teamcity][:admin_user_mongo],
     }
   )
   source(node[:set_value_in_properties_file_powershell_script])
