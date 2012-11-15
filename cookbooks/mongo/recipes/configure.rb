4ba879090be0e1c09f6298eea0e52398eed13f8e
RonaldO <ronald_ocampo@ultimatesoftware.com>
2012-11-15 16:20:03 -0500

    305 - Adding logic to reconfigure replica set on reboot Checking replica set is initialized instead of hard sleep

--- cookbooks/mongo/recipes/configure.rb
+++ cookbooks/mongo/recipes/configure.rb
@@ -1,6 +1,7 @@
 rightscale_marker :begin
 
 database_port = '27017'
+install_directory = node[:platform] == 'ubuntu' ? '/opt/mongodb' : 'c:/mongodb'
 ruby_scripts_dir = node[:ruby_scripts_dir]
 
 if node[:platform] == 'ubuntu'
@@ -13,8 +14,12 @@ else
   template "#{ruby_scripts_dir}/initiate_replica_set.rb" do
     source 'scripts/initiate_replica_set.erb'
     variables(
+      :binaries_directory => node[:binaries_directory],
       :db_port => database_port,
-      :db_replica_set_name => node[:deploy][:db_replica_set_name]
+      :db_replica_set_name => node[:deploy][:db_replica_set_name],
+      :install_directory => install_directory,
+      :service_name => 'mongoDB',
+      :timeout => 120
     )
   end
   powershell 'Initializing the replica set' do
