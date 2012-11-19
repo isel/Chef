891ba4b4713dd020861315b34552917ab11943f8
Isel Fernandez <isel_77@hotmail.com>
2012-11-19 11:02:25 -0500

    adding windows_batch resource

--- cookbooks/windows/recipes/change_administrator_password.rb
+++ cookbooks/windows/recipes/change_administrator_password.rb
@@ -1,7 +1,6 @@
-powershell "change administrator password" do
-  script = <<-EOH
+windows_batch "change administrator password" do
+  code <<-EOH
     net user administrator #{node[:windows][:administrator_password]}
     wmic UserAccount where "Name='administrator'" set PasswordExpires=false
   EOH
-  source(script)
 end
