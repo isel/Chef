55417470d51c4b37c56a677889c66e15db32e201
Ronald Ocampo <ronald_ocampo@ultimatesoftware.com>
2012-11-20 11:08:49 -0500

    296 - debugging params

--- cookbooks/load_balancer/recipes/register_instance_with_haproxy.rb
+++ cookbooks/load_balancer/recipes/register_instance_with_haproxy.rb
@@ -8,10 +8,12 @@ forwarding_ports.each do |port|
       echo registering instance: #{node[:load_balancer][:instance_backend_name]}, ip: #{node[:load_balancer][:instance_ip]}, listener: #{listener_name} with haproxy
 
       script="/opt/rightscale/lb/bin/haproxy_config_server.rb"
-      args="-a add -w -s #{node[:load_balancer][:instance_backend_name]} -l #{listener_name} -t #{node[:load_balancer][:instance_ip]} -e \"inter 3000 rise 2 fall 3 maxconn #{node[:max_connections_per_lb]}\" -k on"
+      args1="-a add -w -s #{node[:load_balancer][:instance_backend_name]}"
+      args2="-l #{listener_name} -t #{node[:load_balancer][:instance_ip]}:#{port}"
+      args3="-e \"inter 3000 rise 2 fall 3 maxconn #{node[:max_connections_per_lb]}\" -k on"
 
-      echo /opt/rightscale/sandbox/bin/ruby $script $args
-      /opt/rightscale/sandbox/bin/ruby $script $args
+      echo /opt/rightscale/sandbox/bin/ruby $script $args1 $args2 $args3
+      /opt/rightscale/sandbox/bin/ruby $script $args1 $args2 $args3
     EOF
   end
 end
