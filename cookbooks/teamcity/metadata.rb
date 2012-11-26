6c73c6b9be5e1e46119ef69bbe1283f7aa85fbf6
Isel Fernandez <isel_77@hotmail.com>
2012-11-26 15:20:25 -0500

    adding recipe to setup teamcity web server volumes

--- cookbooks/teamcity/metadata.rb
+++ cookbooks/teamcity/metadata.rb
@@ -10,6 +10,16 @@ depends 'deploy'
 
 recipe 'teamcity::configure', 'Configures TeamCity build agent properties file'
 
+attribute "core/aws_access_key_id",
+  :display_name => "aws access key id",
+  :required => "required",
+  :recipes => ["teamcity::setup_volumes"]
+
+attribute "core/aws_secret_access_key",
+  :display_name => "aws secret access key",
+  :required => "required",
+  :recipes => ["teamcity::setup_volumes"]
+
 attribute 'deploy/admin_password_mongo',
   :display_name => 'administrator password for mongo',
   :required => 'required',
