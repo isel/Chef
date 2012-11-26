b1bd609cc4b84f1faf70e1621d70faeffc2f154a
Isel Fernandez <isel_77@hotmail.com>
2012-11-26 11:27:36 -0500

    adding cookbook mssql with recipe to set sa password

---
+++ cookbooks/mssql/recipes/set_sa_password.rb
@@ -0,0 +1,9 @@
+powershell('setting sa password') do
+  parameters ({ 'PASSWORD' => node[:mssql][:sa_password] })
+  script <<-EOF
+    add-pssnapin sqlserverCmdletSnapin100
+
+    Invoke-Sqlcmd -Query "ALTER LOGIN [sa] WITH PASSWORD='$env:PASSWORD'"
+  EOF
+  source(script)
+end
