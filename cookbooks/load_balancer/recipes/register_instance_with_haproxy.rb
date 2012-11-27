e2a88983b00a682b8588cccc6f5078f04e00d9c8
Ronald Ocampo <ronald_ocampo@ultimatesoftware.com>
2012-11-27 10:20:04 -0500

    296 - debugging app lb

--- cookbooks/load_balancer/recipes/register_instance_with_haproxy.rb
+++ cookbooks/load_balancer/recipes/register_instance_with_haproxy.rb
@@ -9,6 +9,21 @@ forwarding_ports.each do |port|
 
       script="/opt/rightscale/lb/bin/haproxy_config_server.rb"
 
+      /opt/rightscale/sandbox/bin/ruby $script -a add -w -s #{node[:load_balancer][:instance_backend_name]} -l #{listener_name} -t #{node[:load_balancer][:instance_ip]}:#{port} -e " inter 3000 rise 2 fall 3 maxconn #{node[:max_connections_per_lb]}" -k on
+
+    EOF
+  end
+end
+
+forwarding_ports = node[:load_balancer][:forwarding_ports].split(',').reject{|port| port == '443'}
+forwarding_ports.each do |port|
+  listener_name = "#{node[:load_balancer][:prefix]}#{port}"
+  bash 'Registering instance with haproxy configuration' do
+    code <<-EOF
+      echo registering instance: #{node[:load_balancer][:instance_backend_name]}, ip: #{node[:load_balancer][:instance_ip]}, listener: #{listener_name} with haproxy
+
+      script="/opt/rightscale/lb/bin/haproxy_config_server.rb"
+
       count=0
       echo /opt/rightscale/sandbox/bin/ruby $script -a add -w -s #{node[:load_balancer][:instance_backend_name]} -l #{listener_name} -t #{node[:load_balancer][:instance_ip]}:#{port} -e " inter 3000 rise 2 fall 3 maxconn #{node[:max_connections_per_lb]}" -k on
       result=`/opt/rightscale/sandbox/bin/ruby $script -a add -w -s #{node[:load_balancer][:instance_backend_name]} -l #{listener_name} -t #{node[:load_balancer][:instance_ip]}:#{port} -e " inter 3000 rise 2 fall 3 maxconn #{node[:max_connections_per_lb]}" -k on`
