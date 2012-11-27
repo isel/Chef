b9727683197d9b5a3269c7df021b43bda7d11faa
Isel Fernandez <isel_77@hotmail.com>
2012-11-27 11:23:15 -0500

    adding recipe to schedule backups of the teamcity web server

--- cookbooks/teamcity/recipes/schedule_backups.rb
+++ cookbooks/teamcity/recipes/schedule_backups.rb
@@ -1,9 +1,9 @@
 rightscale_marker :begin
 
-windows_task 'Schedule backups' do
+windows_task 'Backup TeamCity' do
   user 'Administrator'
   password node[:windows][:administrator_password]
-  command 'rs_run_recipe --name "teamcity::back_volumes"'
+  command 'rs_run_recipe --name "teamcity::backup_volumes"'
   run_level :highest
 end
 
